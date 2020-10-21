6f44dda4f42352a016369e7a25a7e9bbf79551fe
Crystal Shaw <c.shaw@ucla.edu>
2020-10-21 13:19:13 -0700

    Select variables of interest

--- preliminary_analyses/HCAP_dimension_reduction.R
+++ preliminary_analyses/HCAP_dimension_reduction.R
@@ -3,6 +3,8 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
+p_load("tidyverse", "magrittr")
+
 #---- source scripts ----
 source(paste0("/Users/CrystalShaw/Desktop/Git Repos/useful-scripts/R/", 
               "data_read/read_da_dct.R"))
@@ -16,5 +18,17 @@ dict_path <- paste0("/Users/CrystalShaw/Box/Dissertation/data/HCAP/HC16/",
 HCAP <- read_da_dct(data_path, dict_path, HHIDPN = "TRUE")
 
 #---- variables of interest ----
-HCAP_vars <- c("HHIDPN", )
+HCAP_vars <- c("HHIDPN", "MSE", "TICS", "WLIMM1", "WLIMM2", "WLIMM3", "1066", 
+               "SCORE")
+not_these <- c("TEST", "INTRO", "H1RMSESCORE", "H1RTICSSCORE", "H1R11066SCORE", 
+               "SCORESE", "CESDSCORE", "STSCORE")
+
+HCAP_assessment <- HCAP %>% dplyr::select(contains(HCAP_vars)) %>% 
+  dplyr::select(-c(contains(not_these), "H1RMSE11T")) #H1RMSE11T1 needs to stay
+
+#---- sum scores and averages of repeated trials ----
+
+
+#---- make correlation matrix ----
+
 
