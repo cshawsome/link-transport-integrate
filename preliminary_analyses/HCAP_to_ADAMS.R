14f5311f892463a1f4026cb001a8a2eaff5aa40e
Crystal Shaw <c.shaw@ucla.edu>
2020-12-07 11:20:19 -0800

    Plot of ADAMS MMSE Mixture distributions code

--- preliminary_analyses/HCAP_to_ADAMS.R
+++ preliminary_analyses/HCAP_to_ADAMS.R
@@ -3,7 +3,7 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "magrittr")
+p_load("tidyverse", "magrittr", "broom")
 
 #---- source scripts ----
 source(paste0("/Users/CrystalShaw/Desktop/Git Repos/useful-scripts/R/", 
@@ -54,6 +54,7 @@ HRS_tracker <- read_da_dct(HCAP_tracker_data_path, HCAP_tracker_dict_path,
 
 #---- join data ----
 HCAP <- left_join(HCAP, HRS_tracker, by = "HHIDPN")
+ADAMS <- left_join(ADAMS_neuropsych_A, ADAMS_tracker, by = "HHIDPN")
 
 #---- select variables ----
 HCAP_vars <- c("HHIDPN", "GENDER", "HISPANIC", "RACE", "SCHLYRS",
@@ -62,13 +63,20 @@ HCAP_vars <- c("HHIDPN", "GENDER", "HISPANIC", "RACE", "SCHLYRS",
                #MMSE
                "H1RMSESCORE")
 
+ADAMS_vars <- c("HHIDPN", 
+                #MMSE
+                "ANMSETOT")
+
 HCAP_subset <- HCAP %>% dplyr::select(all_of(HCAP_vars))
+ADAMS_subset <- ADAMS %>% dplyr::select(all_of(ADAMS_vars))
 
 # #Variable check
 # colSums(is.na(HCAP_subset))
 
 #Drop people without an interview year
 HCAP_subset %<>% filter(!is.na(H1RIWYEAR))
+#Drop people with missing MMSE data in ADAMS
+ADAMS_subset %<>% filter(ANMSETOT <= 30)
 
 #---- format data ----
 #---- **age ----
@@ -161,6 +169,7 @@ ggsave(filename = "HCAP_edyrs.jpeg", plot = last_plot(), device = "jpeg",
 # summary(HCAP_subset$SCHLYRS)
 
 #---- neuropsych distributions ----
+#HCAP
 ggplot(data = HCAP_subset) + 
   geom_histogram(aes(H1RMSESCORE), color = green, fill = green) +
   theme_minimal() + xlab("MMSE Score") + theme(text = element_text(size = 14))
@@ -171,8 +180,100 @@ ggsave(filename = "HCAP_MMSE_score.jpeg", plot = last_plot(), device = "jpeg",
                      "HCAP_synthetic/figures/"), width = 7, height = 5,
        units = "in")
 
+#ADAMS
+ggplot(data = ADAMS_subset) + 
+  geom_histogram(aes(ANMSETOT), color = green, fill = green) +
+  theme_minimal() + xlab("MMSE Score") + theme(text = element_text(size = 14))
+
+ggsave(filename = "ADAMS_MMSE_score.jpeg", plot = last_plot(), device = "jpeg",
+       path = paste0("/Users/CrystalShaw/Box/Dissertation/",
+                     "preliminary_analyses/",
+                     "HCAP_synthetic/figures/"), width = 7, height = 5,
+       units = "in")
+
 # #Variable check
 # summary(HCAP_subset$H1RMSESCORE)
+summary(ADAMS_subset$ANMSETOT)
 
-#---- Modeling HCAP data ----
+#---- modeling HCAP data ----
+#Formatting race/ethnicity factors
+HCAP_subset %<>% mutate_at("race_ethnic_cat", as.factor) %>% 
+  mutate_at("race_ethnic_cat", function(x) fct_relevel(x, "Non-hispanic White"))
+
+#---- **main effects models ----
+MMSE_main_effects <- lm(H1RMSESCORE ~ HCAP_age + female + 
+                          race_ethnic_cat + 
+                          SCHLYRS, data = HCAP_subset)
+
+summary(MMSE_main_effects)
+plot(MMSE_main_effects$residuals)
+hist(MMSE_main_effects$fitted.values)
+summary(MMSE_main_effects$fitted.values)
+
+#---- **2-way interactions ----
+MMSE_2way_intx <- lm(H1RMSESCORE ~ HCAP_age*female + HCAP_age*race_ethnic_cat + 
+                       female*race_ethnic_cat + female*SCHLYRS + 
+                       race_ethnic_cat*SCHLYRS, 
+                     data = HCAP_subset)
+
+summary(MMSE_2way_intx)
+tidy(MMSE_2way_intx, conf.int = TRUE)
+plot(MMSE_2way_intx$residuals)
+hist(MMSE_2way_intx$fitted.values)
+summary(MMSE_2way_intx$fitted.values)
+
+#---- **3-way interactions ----
+MMSE_3way_intx <- lm(H1RMSESCORE ~ HCAP_age*female*race_ethnic_cat + 
+                       SCHLYRS*female*race_ethnic_cat, 
+                     data = HCAP_subset)
+
+summary(MMSE_3way_intx)
+plot(MMSE_3way_intx$residuals)
+hist(MMSE_3way_intx$fitted.values)
+summary(MMSE_3way_intx$fitted.values)
+
+#---- mixture distributions ----
+#Try to recover the HCAP shape distribution without taking covariates into 
+# consideration... yet
+samp_size = 10000
+
+high_score_prop = 0.60; high_score_mean = 27; high_score_SD = 2
+med_score_prop = 0.35; med_score_mean = 22; med_score_SD = 3.5
+low_score_prop = 0.05; low_score_mean = 15; low_score_SD = 5
+
+high_scores <- rnorm(n = floor(high_score_prop*samp_size), 
+                     mean = high_score_mean, sd = high_score_SD)
+high_scores[high_scores > 30] <- 30
+high_scores[high_scores < 0] <- 0
+
+med_scores <- rnorm(n = floor(med_score_prop*samp_size), 
+                     mean = med_score_mean, sd = med_score_SD)
+med_scores[med_scores > 30] <- 30
+med_scores[med_scores < 0] <- 0
+
+low_scores <- rnorm(n = floor(low_score_prop*samp_size), 
+                    mean = low_score_mean, sd = low_score_SD)
+low_scores[low_scores > 30] <- 30
+low_scores[low_scores < 0] <- 0
+
+HCAP_plot_data <- data.frame("values" = HCAP_subset$H1RMSESCORE, 
+                             "data_type" = "HCAP")
+synthetic_HCAP_plot_data <- 
+  data.frame("values" = c(high_scores, med_scores, low_scores), 
+             "data_type" = "Synthetic HCAP") 
+
+plot_data <- rbind(HCAP_plot_data, synthetic_HCAP_plot_data)
+
+#Comparison plot
+ggplot(data = plot_data, aes(x = values, color = data_type, fill = data_type)) + 
+  geom_histogram(alpha = 0.5, position = "identity") + theme_minimal() + 
+  xlab("MMSE Total Scores")
+
+ggsave(filename = "synthetic_HCAP_MMSE_score.jpeg", plot = last_plot(), 
+       device = "jpeg",
+       path = paste0("/Users/CrystalShaw/Box/Dissertation/",
+                     "preliminary_analyses/",
+                     "HCAP_synthetic/figures/"), width = 7, height = 5,
+       units = "in")
+
   
