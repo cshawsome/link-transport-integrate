1a11300eba4d0ffc6a5bbea0c76d584efc303f3b
Crystal Shaw <c.shaw@ucla.edu>
2020-12-29 21:15:05 -0800

    read in and join ADAMS and RAND data

---
+++ RScripts/synthetic_ADAMS.R
@@ -0,0 +1,20 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("tidyverse")
+
+#---- read in data ----
+#---- **ADAMS ----
+ADAMS_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                                "data/cleaned/ADAMS_subset.csv"), 
+                         col_types = cols(HHIDPN = col_character()))
+  
+#---- **RAND ----
+RAND_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                               "data/cleaned/RAND_subset.csv"), 
+                        col_types = cols(HHIDPN = col_character()))
+
+#---- join data ----
+all_data <- left_join(ADAMS_subset, RAND_subset, by = "HHIDPN")
