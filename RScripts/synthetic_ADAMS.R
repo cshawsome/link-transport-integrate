56b95d4ea029915b94bfcc282235455974d2db4b
Crystal Shaw <c.shaw@ucla.edu>
2020-12-29 22:01:57 -0800

    Take SRS of data

--- RScripts/synthetic_ADAMS.R
+++ RScripts/synthetic_ADAMS.R
@@ -24,3 +24,22 @@ vars <- c("HHIDPN", "GENDER", "ETHNIC", "AAGE_cat", "EDYRS_cat", "ANMSETOT_cat",
           paste0("r", c(5, 6, 7), "iadla_cat"))
 
 analytical_sample <- all_data %>% dplyr::select(all_of(vars))
+
+#Variable check
+colSums(is.na(analytical_sample))
+
+#---- Mixture Model ----
+#---- **Step 1: Take SRS of data ----
+samp_size <- floor(0.5*nrow(analytical_sample))
+rsamp <- sample_n(analytical_sample, size = samp_size, replace = FALSE)
+
+#---- **Step 2: Set priors ----
+#number of group-level latent classes
+group_class_n <- 4
+#number of sub-latent classes
+sub_class_n <- 5
+
+
+
+
+
