add8e4a75ca33eb36b52b8c47d23c9131ef05fc0
Crystal Shaw <c.shaw@ucla.edu>
2020-12-31 13:01:52 -0800

    fix indexing on sub-class priors

--- RScripts/synthetic_ADAMS.R
+++ RScripts/synthetic_ADAMS.R
@@ -33,7 +33,7 @@ colSums(is.na(analytical_sample))
 samp_size <- floor(0.5*nrow(analytical_sample))
 rsamp <- sample_n(analytical_sample, size = samp_size, replace = FALSE)
 
-#---- **Step 2: priors ----
+#---- **Step 2: hyperpriors ----
 #---- *** number of latent classes ----
 #number of group-level latent classes
 group_class_n <- 4
@@ -43,11 +43,6 @@ sub_class_n <- 5
 #---- *** p(group-level latent classes) ----
 a_alpha = 1
 b_alpha = 0.5
-
-#Draw priors for one less than the number of group-level latent classes
-u_g <- rgamma(n = (group_class_n - 1), shape = a_alpha, rate = b_alpha)
-#The last u is fixed at 1
-u_g <- c(u_g, 1)
 
 #---- *** p(sub-level latent classes) ----
 #From Dunson and Xing (2009)
@@ -61,8 +56,21 @@ v_g <- rgamma(n = (sub_class_n - 1), shape = a_beta, rate = b_beta)
 #The last v is fixed at 1
 v_g <- c(v_g, 1)
 
-#---- Step 3: draws ----
+#---- Step 3: sampling ----
+#---- **initiate values ----
+#Draw priors for one less than the number of group-level latent classes
+u_g <- rgamma(n = (group_class_n - 1), shape = a_alpha, rate = b_alpha)
+#The last u is fixed at 1
+u_g <- c(u_g, 1)
 
+#Draw priors for one less than the number of sub-level latent classes
+v_g <- rgamma(n = (sub_class_n - 1), shape = a_beta, rate = b_beta)
+#The last v is fixed at 1
+v_g <- c(v_g, 1)
+
+#---- **sample beta ----
+a_beta_post = a_beta + group_class_n*(sub_class_n - 1)
+b_beta_post = 
 
 
 
