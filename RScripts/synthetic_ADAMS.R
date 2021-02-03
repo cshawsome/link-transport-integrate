54cf69a22e1c7c301661b711a1cda83bfb0f65e8
Crystal Shaw <c.shaw@ucla.edu>
2021-02-03 11:32:02 -0800

    add magrittr package

--- RScripts/synthetic_ADAMS.R
+++ RScripts/synthetic_ADAMS.R
@@ -3,7 +3,7 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "DirichletReg")
+p_load("tidyverse", "DirichletReg", "magrittr")
 
 #---- read in data ----
 #---- **ADAMS ----
