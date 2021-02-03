e05d0dc939622a809177dee13b2d9ba6427a22f2
Crystal Shaw <c.shaw@ucla.edu>
2021-02-03 13:34:56 -0800

    mixing continuous and categorical variables

---
+++ RScripts/dataset_contruction_ADAMS_mixed.R
@@ -0,0 +1,188 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("tidyverse", "magrittr")
+
+#---- source scripts ----
+source(paste0("/Users/CrystalShaw/Desktop/Git Repos/useful-scripts/R/", 
+              "data_read/read_da_dct.R"))
+
+#---- import data ----
+#---- **neuropsych ----
+for(wave in c("a", "b", "c", "d")){
+  neuropsych_data_path <- paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
+                                 "ADAMS/adams1", wave, "/adams1", wave, "da/", 
+                                 "ADAMS1", str_to_upper(wave), "N_R.da")
+  
+  neuropsych_dict_path <- paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
+                                 "ADAMS/adams1", wave, "/adams1", wave, "sta/", 
+                                 "ADAMS1", str_to_upper(wave), "N_R.dct")
+  
+  if(wave == "a"){
+    ADAMS_neuropsych <- read_da_dct(neuropsych_data_path, neuropsych_dict_path, 
+                                    HHIDPN = "TRUE") 
+  } else{
+    ADAMS_neuropsych %<>% 
+      left_join(., read_da_dct(neuropsych_data_path, neuropsych_dict_path, 
+                               HHIDPN = "TRUE"), 
+                by = "HHIDPN")
+  }
+}
+
+#---- **sociodemographic ----
+ADAMS_tracker_data_path <- 
+  paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
+         "ADAMS/adams1trk/ADAMS1TRK_R.da")
+ADAMS_tracker_dict_path <- 
+  paste0("/Users/CrystalShaw/Box/Dissertation/data/",
+         "ADAMS/adams1trk/adams1trksta/ADAMS1TRK_R.dct")
+
+#filter to those who completed Wave A assessment
+ADAMS_tracker <- read_da_dct(ADAMS_tracker_data_path, ADAMS_tracker_dict_path, 
+                             HHIDPN = "TRUE") %>% filter(AASSESS == 1) 
+#---- **dem dx ----
+for(wave in c("a", "b", "c", "d")){
+  demdx_data_path <- paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
+                            "ADAMS/adams1", wave, "/adams1", wave, "da/", 
+                            "ADAMS1", str_to_upper(wave), "D_R.da")
+  demdx_dict_path <- paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
+                            "ADAMS/adams1", wave, "/adams1", wave, "sta/", 
+                            "ADAMS1", str_to_upper(wave), "D_R.dct")
+  if(wave == "a"){
+    ADAMS_demdx <- read_da_dct(demdx_data_path, demdx_dict_path, 
+                               HHIDPN = "TRUE") %>% 
+      dplyr::select("HHIDPN", paste0(str_to_upper(wave), "DFDX1")) 
+  } else{
+    ADAMS_demdx %<>% 
+      left_join(., read_da_dct(demdx_data_path, demdx_dict_path, 
+                               HHIDPN = "TRUE") %>% 
+                  dplyr::select("HHIDPN", 
+                                paste0(str_to_upper(wave), "DFDX1")))
+  }
+}
+
+for(wave in c("A", "B", "C", "D")){
+  dem_dx_var <- paste0(wave, "DFDX1")
+  ADAMS_demdx[, paste0(wave, "dem_dx_cat")] <- 
+    case_when(ADAMS_demdx[, dem_dx_var] %in% c(1, 2) ~ "Probable/Possible AD", 
+              ADAMS_demdx[, dem_dx_var] %in% c(3, 4) ~ 
+                "Probable/Possible Vascular Dementia", 
+              ADAMS_demdx[, dem_dx_var] %in% 
+                c(5, 6, 7, 8, 11, 14, 23, 24, 25, 26, 27, 21, 28, 29, 
+                  30, 33) ~ "Other",
+              ADAMS_demdx[, dem_dx_var] %in% c(18, 32) ~ "Probable Dementia",
+              ADAMS_demdx[, dem_dx_var] %in% c(10, 13, 15, 16, 17, 19) ~ 
+                "Dementia", 
+              ADAMS_demdx[, dem_dx_var] %in% c(20, 22) ~ "MCI", 
+              ADAMS_demdx[, dem_dx_var] == 31 ~ "Normal")
+}
+
+# #Sanity check
+# for(wave in c("A", "B", "C", "D")){
+#   dem_dx_var <- paste0(wave, "DFDX1")
+#   print(paste0("Wave ", wave))
+#   print(table(ADAMS_demdx[, dem_dx_var], 
+#               ADAMS_demdx[, paste0(wave, "dem_dx_cat")], 
+#               useNA = "ifany"))
+# }
+
+#Remove original variables
+ADAMS_demdx %<>% dplyr::select(-c(paste0(c("A", "B", "C", "D"), "DFDX1")))
+
+#---- join data ----
+ADAMS <- left_join(ADAMS_tracker, ADAMS_neuropsych, by = "HHIDPN") %>% 
+  left_join(., ADAMS_demdx, by = "HHIDPN")
+
+#---- select vars ----
+vars <- c("HHIDPN",
+          #ADAMS interview year
+          "AYEAR",
+          #Sociodemographics
+          "AGE", "GENDER", "ETHNIC", "EDYRS",
+          #Cognition
+          "NMSETOT", 
+          #Dem dx
+          "dem_dx_cat")
+
+ADAMS_subset <- ADAMS %>% dplyr::select(contains(all_of(vars))) %>% 
+  #Drop age bracket, age at selection, and language variables
+  dplyr::select(-contains(c("BKT", "AGESEL", "LANGUAGE")))
+
+ADAMS_waves <- c("A", "B", "C", "D")
+
+#---- variable check ----
+#We need to turn these variables into categories >= 1 
+table(ADAMS_subset$GENDER, useNA = "ifany")
+table(ADAMS_subset$ETHNIC, useNA = "ifany")
+table(ADAMS_subset$EDYRS, useNA = "ifany")
+
+#---- clean: Sociodemographics ----
+#---- **age ----
+for(wave in ADAMS_waves){
+  col <- paste0(wave, "AGE")
+  new_col <- paste0(col, "_cat")
+  
+  ADAMS_subset[, new_col] <- ADAMS_subset[, col] - 
+    (min(ADAMS_subset[, col], na.rm = TRUE) - 1)
+  
+  ADAMS_subset[which(ADAMS_subset[, new_col] > 900), new_col] <- NA
+}
+
+# #Sanity check
+# for(wave in ADAMS_waves){
+#   col <- paste0(wave, "AGE")
+#   new_col <- paste0(col, "_cat")
+#   
+#   print(paste0("Wave", wave))
+#   print(table(ADAMS_subset[, new_col], ADAMS_subset[, col], useNA = "ifany"))
+# }
+
+#---- **edyrs ----
+ADAMS_subset %<>% mutate(EDYRS_cat = EDYRS + 1)
+
+# #Sanity check
+# table(ADAMS_subset$EDYRS, ADAMS_subset$EDYRS_cat, useNA = "ifany")
+
+#---- clean: neuropsych ----
+#---- **MMSE ----
+#Variable check
+for(wave in ADAMS_waves){
+  MMSE_var <- paste0(wave, "NMSETOT")
+  print(paste0("Wave ", wave))
+  print(table(ADAMS_subset[, MMSE_var], useNA = "ifany"))
+}
+
+for(wave in ADAMS_waves){
+  MMSE_var <- paste0(wave, "NMSETOT")
+  new_var <- paste0(wave, "NMSETOT_cat")
+  
+  ADAMS_subset[, new_var] <- ADAMS_subset[, MMSE_var] + 1
+}
+
+ADAMS_subset %<>% 
+  mutate_at(.vars = paste0(c("A", "B", "C", "D"), "NMSETOT_cat"), 
+            function(x) ifelse(x > 31, NA, x))
+
+#Sanity check
+for(wave in ADAMS_waves){
+  MMSE_var <- paste0(wave, "NMSETOT")
+  new_var <- paste0(wave, "NMSETOT_cat")
+  
+  print(paste0("Wave ", wave))
+  print(table(ADAMS_subset[, new_var],
+              ADAMS_subset[, MMSE_var], useNA = "ifany"))
+}
+
+#---- drop original variables ----
+drop_these <- c("EDYRS", paste0(ADAMS_waves, "AGE"), 
+                paste0(ADAMS_waves, "NMSETOT"))
+
+ADAMS_subset %<>% dplyr::select(-all_of(drop_these))
+
+#---- save dataset ----
+write_csv(ADAMS_subset, file = paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                                      "data/cleaned/ADAMS_subset.csv"))
+
+
