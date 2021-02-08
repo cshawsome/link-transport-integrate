ca9c24b1be470a4570c6246193cf990b56625f7f
Crystal Shaw <c.shaw@ucla.edu>
2021-02-08 10:39:15 -0800

    read in and format data

---
+++ RScripts/modeling_ADAMS_complete_mixed.R
@@ -0,0 +1,44 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("tidyverse", "DirichletReg", "magrittr")
+
+#---- read in data ----
+#---- **ADAMS ----
+ADAMS_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                                "data/cleaned/ADAMS_subset_mixed.csv"), 
+                         col_types = cols(HHIDPN = col_character()))
+
+#---- **RAND ----
+RAND_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                               "data/cleaned/RAND_subset.csv"), 
+                        col_types = cols(HHIDPN = col_character()))
+
+#---- join data ----
+all_data <- left_join(ADAMS_subset, RAND_subset, by = "HHIDPN")
+
+#---- select variables ----
+vars <- c("GENDER", "ETHNIC", "log_AAGE", "EDYRS", 
+          "ANMSETOT", paste0("r", seq(5, 7), "iadla_cat"))
+
+analytical_sample <- all_data %>% 
+  dplyr::select("HHIDPN", "AYEAR", 
+                all_of(vars)) %>% na.omit()
+
+#Variable check-- there's 538 people in the complete data set
+colSums(is.na(analytical_sample))
+dim(analytical_sample)
+
+#---- **IADLA ----
+#Take the IADLA measure closest to ADAMS interview year
+analytical_sample %<>% 
+  mutate("IADLA" = case_when(AYEAR == 2001 ~ r5iadla_cat, 
+                             AYEAR %in% c(2002, 2003) ~ r6iadla_cat, 
+                             AYEAR == 2004 ~ r7iadla_cat))
+# #Sanity check
+# table(analytical_sample$IADLA, useNA = "ifany")
+
+#Get rid of original variables
+analytical_sample %<>% dplyr::select(-c(contains("iadla_cat"), "AYEAR")) 
