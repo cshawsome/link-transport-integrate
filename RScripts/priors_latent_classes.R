c33a55597693eab1324f191c5d25a37039b9490b
Crystal Shaw <c.shaw@ucla.edu>
2021-02-24 20:58:32 -0800

    indicators for different levels of impairment

--- RScripts/priors_latent_classes.R
+++ RScripts/priors_latent_classes.R
@@ -11,3 +11,15 @@ ADAMS_subset <-
                   "data/cleaned/ADAMS_subset_mixed.csv"))
 
 #---- dem class indicators ----
+ADAMS_subset %<>% 
+  #Impaired vs. Normal
+  mutate("ANormal" = ifelse(Adem_dx_cat == "Normal", 1, 0),
+         #Other vs. Dementia/MCI
+         "AOther" = ifelse(Adem_dx_cat == "Other", 1, 0), 
+         #MCI vs. Dementia
+         "AMCI" = ifelse(Adem_dx_cat == "MCI", 1, 0))
+
+# #Sanity check
+# table(ADAMS_subset$Adem_dx_cat, ADAMS_subset$ANormal, useNA = "ifany")
+# table(ADAMS_subset$Adem_dx_cat, ADAMS_subset$AOther, useNA = "ifany")
+# table(ADAMS_subset$Adem_dx_cat, ADAMS_subset$AMCI, useNA = "ifany")
