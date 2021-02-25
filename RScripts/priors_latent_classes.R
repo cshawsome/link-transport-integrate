164d730d5d36dca7a43b0f2fc6ddeed54f399c21
Crystal Shaw <c.shaw@ucla.edu>
2021-02-24 20:52:00 -0800

    read in cleaned ADAMS data

---
+++ RScripts/priors_latent_classes.R
@@ -0,0 +1,13 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("tidyverse", "magrittr")
+
+#---- read in data ----
+ADAMS_subset <- 
+  read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                  "data/cleaned/ADAMS_subset_mixed.csv"))
+
+#---- dem class indicators ----
