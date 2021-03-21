8d5bb6dfc5f3a3315211d365ad1301c4df9c5728
Crystal Shaw <c.shaw@ucla.edu>
2021-03-21 10:39:23 -0700

    only read in ADAMS columns of interest

--- RScripts/results_plots.R
+++ RScripts/results_plots.R
@@ -6,11 +6,21 @@ if (!require("pacman")){
 p_load("tidyverse", "magrittr", "here", "wesanderson", "RColorBrewer")
 
 #---- read in data ----
-ADAMS_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                                "data/cleaned/ADAMS_subset_mixed.csv"), 
-                         col_types = cols(HHIDPN = col_character()))
-
 synthetic_ADAMS <- 
   read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
                   "analyses/results/ADAMSA/ADAMSA_synthetic.csv"), 
            col_types = cols(HHIDPN = col_character()))
+
+ADAMS_columns <- c(colnames(synthetic_ADAMS)[
+  which(!colnames(synthetic_ADAMS) %in% 
+          c("(Intercept)", "Group", "Black", "Hispanic", "p_Unimpaired", 
+            "p_Other", "p_MCI"))], "Adem_dx_cat")
+
+ADAMS_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                                "data/cleaned/ADAMS_subset_mixed.csv"), 
+                         col_types = cols(HHIDPN = col_character())) %>% 
+  #only take those part of modeling (complete cases only for now)
+  filter(HHIDPN %in% synthetic_ADAMS$HHIDPN) %>% 
+  dplyr::select(all_of(ADAMS_columns))
+  
+#---- 
