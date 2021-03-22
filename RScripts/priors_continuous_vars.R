d23a6c3eaf12d2a1f758e8c067d4b955da45d2ef
Crystal Shaw <c.shaw@ucla.edu>
2021-03-22 12:20:22 -0700

    don't change characters to factors

--- RScripts/priors_continuous_vars.R
+++ RScripts/priors_continuous_vars.R
@@ -10,6 +10,18 @@ options(scipen = 999)
 #---- read in data ----
 ADAMS_subset <- 
   read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                  "data/cleaned/ADAMS_subset_mixed.csv")) %>% 
-  mutate_if(is.character, as.factor)
+                  "data/cleaned/ADAMS_subset_mixed.csv")) 
+
+#---- data cleaning: dem dx ----
+ADAMS_subset %<>% 
+  mutate("Adem_dx_cat_collapse" = 
+           case_when(Adem_dx_cat %in% 
+                       c("Dementia", "Probable/Possible AD", "Probable Dementia", 
+                         "Probable/Possible Vascular Dementia") ~ "Dementia", 
+                     TRUE ~ Adem_dx_cat))
+
+# #Sanity check
+# table(ADAMS_subset$Adem_dx_cat, ADAMS_subset$Adem_dx_cat_collapse,
+#       useNA = "ifany")
+
 
