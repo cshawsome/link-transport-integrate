a6aabb295bc5ec0dc5e61d1573a5c7f2107449b8
Crystal Shaw <c.shaw@ucla.edu>
2021-03-27 19:27:30 -0700

    use pre-defined vectors to select ADAMS subset

--- RScripts/priors_continuous_vars.R
+++ RScripts/priors_continuous_vars.R
@@ -3,19 +3,23 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "magrittr", "broom", "openxlsx", "sjPlot", "here")
+p_load("tidyverse", "magrittr", "here", "MCMCpack")
 
 options(scipen = 999)
 
 #---- read in data ----
+#Categorical vars (notation from Schafer 1997)
+W <- c("ETHNIC_label", "Astroke")
 #Continuous vars (notation from Schafer 1997)
 Z <- c("AAGE", "ANMSETOT", "ANSER7T", "ANIMMCR", "ANRECYES", "ANWM1TOT", 
        "proxy_cog", "ANDELCOR", "Aiadla", "Abmi")
 
+group <- c("Adem_dx_cat")
+
 ADAMS_subset <- 
   read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
                   "data/cleaned/ADAMS_subset_mixed.csv")) %>% 
-  dplyr::select()
+  dplyr::select(c("HHIDPN", all_of(group), all_of(W), all_of(Z)))
 
 #---- data cleaning: dem dx ----
 ADAMS_subset %<>% 
@@ -32,4 +36,8 @@ ADAMS_subset %<>%
 #---- Sigma priors ----
 test <- ADAMS_subset %>% filter(Adem_dx_cat_collapse == "Normal")
 
+test_sigma <- var(test %>% dplyr::select(all_of(Z)) %>% na.omit() %>% 
+                    as.matrix())
+
+test_random <- riwish(v = nrow(test_sigma), S = test_sigma)
 
