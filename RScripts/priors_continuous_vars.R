7f32d340679629d919c7f7e25fbc90897bcd751d
Crystal Shaw <c.shaw@ucla.edu>
2021-05-17 16:00:39 -0700

    go through groups numerically for pooling UtU

--- RScripts/priors_continuous_vars.R
+++ RScripts/priors_continuous_vars.R
@@ -18,7 +18,11 @@ group <- c("Adem_dx_cat")
 
 ADAMS_subset <- 
   read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                  "data/cleaned/ADAMS/ADAMS_train.csv")) 
+                  "data/cleaned/ADAMS/ADAMS_train.csv")) %>% 
+  mutate("group_number" = case_when(Adem_dx_cat == "Normal" ~ 1, 
+                                    Adem_dx_cat == "Other" ~ 2, 
+                                    Adem_dx_cat == "MCI" ~ 3, 
+                                    Adem_dx_cat == "Dementia" ~ 4))
 
 #---- arrange data ----
 ADAMS_subset %<>% arrange(Astroke, desc(Black), desc(Hispanic))
@@ -43,20 +47,23 @@ cells <-
 B = 10000
 priors_beta <- as.data.frame(matrix(nrow = (10*4*4) , ncol = B)) %>% 
   set_colnames(seq(1, B)) %>% 
-  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 40)) 
+  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 40), 
+         "group_number" = rep(c(4, 3, 2, 1), each = 40)) 
 priors_V_inv <- as.data.frame(matrix(nrow = (4*4*4), ncol = B)) %>% 
   set_colnames(seq(1, B)) %>% 
-  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 16)) 
+  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 16), 
+         "group_number" = rep(c(4, 3, 2, 1), each = 16)) 
 priors_Sigma <- as.data.frame(matrix(nrow = (10*10*4), ncol = B)) %>% 
   set_colnames(seq(1, B)) %>% 
-  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 100)) 
+  mutate("group" = rep(unique(ADAMS_subset$Adem_dx_cat), each = 100), 
+         "group_number" = rep(c(4, 3, 2, 1), each = 100)) 
 
 for(b in 1:B){
   sample <- sample_n(ADAMS_subset, size = nrow(ADAMS_subset), replace = TRUE)
   
-  for(group in unique(ADAMS_subset$Adem_dx_cat)){
+  for(group in seq(1, 4)){
     #---- filter data ----
-    subset <- sample %>% filter(Adem_dx_cat == group) %>% 
+    subset <- sample %>% filter(group_number == group) %>% 
       arrange(Astroke, desc(Black), desc(Hispanic))
     
     #---- U (contingency cell) ----
@@ -87,18 +94,33 @@ for(b in 1:B){
     
     UtU <- diag(unlist(contingency_table[, "Freq"]))
     
+    if(group %in% c(2, 3)){
+      assign(paste0("UtU_", group), UtU)
+    }
+    
+    #---- ****pool UtU if needed ----
+    if(det(t(A) %*% UtU %*% A) == 0){
+      if(exists(paste0("UtU_", (i-1)))){
+        UtU <- UtU + get(paste0("UtU_", (i-1)))
+      } else{
+        UtU <- UtU + get(paste0("UtU_", (i+1))) 
+      }
+    }
+    
     #---- beta hat ----
     continuous_covariates <- subset %>% dplyr::select(all_of(Z)) %>% as.matrix
     
     V_inv <- t(A) %*% UtU %*% A
     V <- solve(V_inv)
-    priors_V_inv[which(priors_V_inv$group == group), b] <- as.vector(V_inv)
+    priors_V_inv[which(priors_V_inv$group_number == group), b] <- 
+      as.vector(V_inv)
     
     beta_hat <- V %*% t(A) %*% t(U) %*% continuous_covariates
-    priors_beta[which(priors_beta$group == group), b] <- as.vector(beta_hat)
+    priors_beta[which(priors_beta$group_number == group), b] <- 
+      as.vector(beta_hat)
     
     residual <- continuous_covariates - U %*% A %*% beta_hat
-    priors_Sigma[which(priors_Sigma$group == group), b] <- 
+    priors_Sigma[which(priors_Sigma$group_number == group), b] <- 
       as.vector(t(residual) %*% residual)
   }
 }
