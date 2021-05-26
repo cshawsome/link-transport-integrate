ec6c45bfed1ccb6a5f1738868bd8d0eb3148a920
Crystal Shaw <c.shaw@ucla.edu>
2021-05-25 17:31:18 -0700

    read in ADAMS data and synthetic data

--- RScripts/posterior_predictive_checks.R
+++ RScripts/posterior_predictive_checks.R
@@ -6,3 +6,23 @@ if (!require("pacman")){
 p_load("tidyverse", "magrittr", "here", "wesanderson", "RColorBrewer")
 
 #---- read in data ----
+#---- **ADAMS ----
+ADAMS_train <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                               "data/cleaned/ADAMS/ADAMS_train.csv"), 
+                        col_types = cols(HHIDPN = col_character())) 
+
+#---- **synthetic ----
+for(run in 1:10){
+  if(run == 1){
+    synthetic_sample <- 
+      read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
+                      "results/ADAMSA/standard_normal/ADAMSA_synthetic_", run, 
+                      ".csv")) %>% mutate("sample" = run)
+  } else{
+    synthetic_sample %<>% 
+      rbind(., 
+            read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
+                            "results/ADAMSA/standard_normal/ADAMSA_synthetic_", 
+                            run, ".csv")) %>% mutate("sample" = run))
+  }
+}
