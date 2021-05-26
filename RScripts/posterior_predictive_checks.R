20df7f48b99a06292459b2891ddb928a2e6303a6
Crystal Shaw <c.shaw@ucla.edu>
2021-05-25 17:23:52 -0700

    initial commit

---
+++ RScripts/posterior_predictive_checks.R
@@ -0,0 +1,8 @@
+#---- package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos = 'http://cran.us.r-project.org')
+}
+
+p_load("tidyverse", "magrittr", "here", "wesanderson", "RColorBrewer")
+
+#---- read in data ----
