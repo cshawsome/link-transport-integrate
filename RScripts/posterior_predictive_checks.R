fdf3cf7aa71abbc85f8b26f02d60391a509207a9
Crystal Shaw <c.shaw@ucla.edu>
2021-05-30 09:48:55 -0700

    median and skew assessments

--- RScripts/posterior_predictive_checks.R
+++ RScripts/posterior_predictive_checks.R
@@ -3,10 +3,11 @@ if (!require("pacman")){
   install.packages("pacman", repos = 'http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "magrittr", "here", "wesanderson", "RColorBrewer")
+p_load("tidyverse", "magrittr", "here", "wesanderson", "RColorBrewer", 
+       "moments")
 
 #---- read in data ----
-#---- **ADAMS ----
+#---- **ADAMS train ----
 ADAMS_train <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
                                "data/cleaned/ADAMS/ADAMS_train.csv"), 
                         col_types = cols(HHIDPN = col_character())) 
@@ -18,6 +19,15 @@ Z <- cbind(c("AAGE", "ANMSETOT", "ANSER7T", "ANIMMCR", "ANRECYES", "ANWM1TOT",
              "Wordlist Recall (Yes)", "Story Recall I", "Proxy Cognition (Avg)", 
              "Delayed Word Recall", "IADLs", "BMI")) %>% 
   set_colnames(c("var", "label"))
+
+#---- **ADAMS unscaled ----
+ADAMS_subset <- read_csv(paste0("/Users/CrystalShaw/Box/Dissertation/", 
+                               "data/cleaned/ADAMS/ADAMS_subset_mixed.csv"), 
+                        col_types = cols(HHIDPN = col_character())) %>% 
+  filter(HHIDPN %in% ADAMS_train$HHIDPN)
+
+ADAMS_means <- colMeans(ADAMS_subset %>% dplyr::select(all_of(Z[, "var"])))
+ADAMS_sds <- apply(ADAMS_subset %>% dplyr::select(all_of(Z[, "var"])), 2, sd)
 
 #---- **synthetic ----
 for(run in 1:10){
@@ -115,13 +125,199 @@ for(dem_group in unique(synthetic_count_plot_data$group)){
 }
 
 #---- continuous checks ----
-#---- **density plots ----
-#---- **median plots ----
-synthetic_continuous <- matrix(0, nrow = 6*, ncol = (num_samples + 2)) %>% 
-  as.data.frame() %>% set_colnames(c(seq(1, num_samples), "group", "cell"))
-synthetic_counts[, "group"] <- rep(unique(ADAMS_train$Adem_dx_cat), each = 6)
+#---- **density ----
+#---- **median ----
+synthetic_continuous <- 
+  matrix(0, nrow = nrow(Z)*4, ncol = (num_samples + 2)) %>% 
+  as.data.frame() %>% set_colnames(c(seq(1, num_samples), "group", "var"))
+synthetic_continuous[, "group"] <- rep(seq(1, 4), each = nrow(Z))
+synthetic_continuous[, "var"] <- rep(Z[, "var"], 4)
 
-#---- **skew plots ----
+#medians from synthetic datasets
+for(group in 1:4){
+  subsample <- synthetic_sample %>% filter(Group == group)
+  
+  for(var in Z[, "var"]){
+    subset <- subsample %>% dplyr::select(!!as.symbol(var), "sample") 
+    subset[, var] <- subset[, var]*ADAMS_sds[var] + ADAMS_means[var]
+    synthetic_continuous[which(synthetic_continuous$group == group & 
+                                 synthetic_continuous$var == var), 
+                         seq(1, num_samples)] <- 
+      t(subset %>% group_by(sample) %>% summarize_all(median) %>% 
+          dplyr::select(var))
+  }
+}
+
+synthetic_continuous %<>% 
+  mutate("truth" = 0, 
+         "label" = rep(Z[, "label"], 4), 
+         "group_label" = case_when(group == 1 ~ "Normal", 
+                                   group == 2 ~ "Other", 
+                                   group == 3 ~ "MCI", 
+                                   group == 4 ~ "Dementia")) %>% 
+  mutate("color" = case_when(group_label == "Normal" ~ "#00a389", 
+                             group_label == "Other" ~ "#28bed9", 
+                             group_label == "MCI" ~ "#fdab00", 
+                             group_label == "Dementia" ~ "#ff0000"))
+
+for(group in unique(ADAMS_subset$Adem_dx_cat)){
+  subsample <- ADAMS_subset %>% filter(Adem_dx_cat == group)
+  
+  for(var in Z[, "var"]){
+    synthetic_continuous[which(synthetic_continuous$group_label == group & 
+                                 synthetic_continuous$var == var), 
+                         "truth"] <- median(unlist(subsample[, var]))
+  }
+}
+
+synthetic_continuous %<>% pivot_longer(seq(1:num_samples))
+
+#---- ****plot ----
+for(dem_group in unique(synthetic_continuous$group_label)){
+  for(var_name in Z[, "label"]){
+    subset <- synthetic_continuous %>% 
+      filter(group_label == dem_group & label == var_name)
+    ggplot(data = subset , aes(x = value)) + 
+      geom_histogram(fill = "black", color = "black") + theme_minimal() + 
+      xlab("Median") + ggtitle(var_name) + 
+      geom_vline(xintercept = subset$truth, color = unique(subset$color), 
+                 size = 2)
+    
+    ggsave(filename = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                             "posteriors/continuous_vars/median/", 
+                             tolower(dem_group), "/", tolower(dem_group), "_", 
+                             var_name, ".jpeg"), 
+           width = 5, height = 3, units = "in")
+  } 
+}
+
+#---- **skew ----
+synthetic_continuous <- 
+  matrix(0, nrow = nrow(Z)*4, ncol = (num_samples + 2)) %>% 
+  as.data.frame() %>% set_colnames(c(seq(1, num_samples), "group", "var"))
+synthetic_continuous[, "group"] <- rep(seq(1, 4), each = nrow(Z))
+synthetic_continuous[, "var"] <- rep(Z[, "var"], 4)
+
+#skewness from synthetic datasets
+for(group in 1:4){
+  subsample <- synthetic_sample %>% filter(Group == group)
+  
+  for(var in Z[, "var"]){
+    subset <- subsample %>% dplyr::select(!!as.symbol(var), "sample") 
+    subset[, var] <- subset[, var]*ADAMS_sds[var] + ADAMS_means[var]
+    synthetic_continuous[which(synthetic_continuous$group == group & 
+                                 synthetic_continuous$var == var), 
+                         seq(1, num_samples)] <- 
+      t(subset %>% group_by(sample) %>% summarize_all(skewness) %>% 
+          dplyr::select(var))
+  }
+}
+
+synthetic_continuous %<>% 
+  mutate("truth" = 0, 
+         "label" = rep(Z[, "label"], 4), 
+         "group_label" = case_when(group == 1 ~ "Normal", 
+                                   group == 2 ~ "Other", 
+                                   group == 3 ~ "MCI", 
+                                   group == 4 ~ "Dementia")) %>% 
+  mutate("color" = case_when(group_label == "Normal" ~ "#00a389", 
+                             group_label == "Other" ~ "#28bed9", 
+                             group_label == "MCI" ~ "#fdab00", 
+                             group_label == "Dementia" ~ "#ff0000"))
+
+for(group in unique(ADAMS_subset$Adem_dx_cat)){
+  subsample <- ADAMS_subset %>% filter(Adem_dx_cat == group)
+  
+  for(var in Z[, "var"]){
+    synthetic_continuous[which(synthetic_continuous$group_label == group & 
+                                 synthetic_continuous$var == var), 
+                         "truth"] <- skewness(unlist(subsample[, var]))
+  }
+}
+
+synthetic_continuous %<>% pivot_longer(seq(1:num_samples))
+
+#---- ****plot ----
+for(dem_group in unique(synthetic_continuous$group_label)){
+  for(var_name in Z[, "label"]){
+    subset <- synthetic_continuous %>% 
+      filter(group_label == dem_group & label == var_name)
+    ggplot(data = subset , aes(x = value)) + 
+      geom_histogram(fill = "black", color = "black") + theme_minimal() + 
+      xlab("Skew") + ggtitle(var_name) + 
+      geom_vline(xintercept = subset$truth, color = unique(subset$color), 
+                 size = 2)
+    
+    ggsave(filename = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                             "posteriors/continuous_vars/skew/", 
+                             tolower(dem_group), "/", tolower(dem_group), "_", 
+                             var_name, ".jpeg"), 
+           width = 5, height = 3, units = "in")
+  } 
+}
 
 #---- impairment classification ----
+ <- 
+  matrix(0, nrow = nrow(Z)*4, ncol = (num_samples + 2)) %>% 
+  as.data.frame() %>% set_colnames(c(seq(1, num_samples), "group", "var"))
+synthetic_continuous[, "group"] <- rep(seq(1, 4), each = nrow(Z))
+synthetic_continuous[, "var"] <- rep(Z[, "var"], 4)
+
+#medians from synthetic datasets
+for(group in 1:4){
+  subsample <- synthetic_sample %>% filter(Group == group)
+  
+  for(var in Z[, "var"]){
+    subset <- subsample %>% dplyr::select(!!as.symbol(var), "sample") 
+    subset[, var] <- subset[, var]*ADAMS_sds[var] + ADAMS_means[var]
+    synthetic_continuous[which(synthetic_continuous$group == group & 
+                                 synthetic_continuous$var == var), 
+                         seq(1, num_samples)] <- 
+      t(subset %>% group_by(sample) %>% summarize_all(median) %>% 
+          dplyr::select(var))
+  }
+}
+
+synthetic_continuous %<>% 
+  mutate("truth" = 0, 
+         "label" = rep(Z[, "label"], 4), 
+         "group_label" = case_when(group == 1 ~ "Normal", 
+                                   group == 2 ~ "Other", 
+                                   group == 3 ~ "MCI", 
+                                   group == 4 ~ "Dementia")) %>% 
+  mutate("color" = case_when(group_label == "Normal" ~ "#00a389", 
+                             group_label == "Other" ~ "#28bed9", 
+                             group_label == "MCI" ~ "#fdab00", 
+                             group_label == "Dementia" ~ "#ff0000"))
+
+for(group in unique(ADAMS_subset$Adem_dx_cat)){
+  subsample <- ADAMS_subset %>% filter(Adem_dx_cat == group)
+  
+  for(var in Z[, "var"]){
+    synthetic_continuous[which(synthetic_continuous$group_label == group & 
+                                 synthetic_continuous$var == var), 
+                         "truth"] <- median(unlist(subsample[, var]))
+  }
+}
+
+synthetic_continuous %<>% pivot_longer(seq(1:num_samples))
+
+#---- ****plot ----
+for(dem_group in unique(synthetic_continuous$group_label)){
+  for(var_name in Z[, "label"]){
+    subset <- synthetic_continuous %>% 
+      filter(group_label == dem_group & label == var_name)
+    ggplot(data = subset , aes(x = value)) + 
+      geom_histogram(fill = "black", color = "black") + theme_minimal() + 
+      xlab("Median") + ggtitle(var_name) + 
+      geom_vline(xintercept = subset$truth, color = unique(subset$color), 
+                 size = 2)
+    
+    ggsave(filename = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                             "posteriors/continuous_vars/median/", 
+                             tolower(dem_group), "/", tolower(dem_group), "_", 
+                             var_name, ".jpeg"), 
+           width = 5, height = 3, units = "in")
+  } 
+}
 
