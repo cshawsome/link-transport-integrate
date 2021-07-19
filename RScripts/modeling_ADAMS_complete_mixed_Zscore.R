3d43ae4b7f408f9287b7f7045f25eb4164713016
Crystal Shaw <c.shaw@ucla.edu>
2021-07-18 17:20:30 -0700

    remove old code

--- RScripts/modeling_ADAMS_complete_mixed_Zscore.R
+++ RScripts/modeling_ADAMS_complete_mixed_Zscore.R
@@ -329,7 +329,7 @@ extended_pallette14 <- colorRampPalette(wes_palette("Darjeeling1"))(14)
 extended_pallette6 <- colorRampPalette(wes_palette("Darjeeling1"))(6)
 
 #---- **gamma chains ----
-gamma_plot_data <- model_gamma_chain %>% as.data.frame() %>% 
+gamma_plot_data <- results$model_gamma_chain %>% as.data.frame() %>% 
   set_colnames(c(seq(1:B), "model", "pred")) %>%
   pivot_longer(cols = paste0(seq(1:B)), 
                names_to = "Run", values_to = "gamma") %>% 
@@ -453,31 +453,6 @@ ggsave(filename = "mu_chain.jpeg", plot = mu_chain_plot,
        path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
                      "standard_normal"), 
        width = 14, height = 10, units = "in", device = "jpeg")
-
-# #---- **varY chain ----
-# varY_chain_data <- varY_chain %>% as.data.frame() %>% 
-#   rownames_to_column("Z") %>% 
-#   pivot_longer(-c("Z"), names_to = c("Group", "Run"), names_sep = ":", 
-#                values_to = "variance") %>% 
-#   mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
-#                                    Group == 2 ~ "Other", Group == 3 ~ "MCI", 
-#                                    Group == 4 ~ "Dementia")) %>%
-#   mutate_at("Run", as.numeric) %>%
-#   mutate_if(is.character, as.factor) 
-# 
-# varY_chain_plot <- ggplot(data = varY_chain_data, 
-#                           aes(x = Run, y = variance, colour = Z)) +       
-#   geom_line(aes(group = Z)) + 
-#   theme_minimal() + xlab("Run") + ylab("Variance") +  
-#   scale_color_manual(values = rev(extended_pallette14)) + 
-#   scale_x_continuous(breaks = seq(0, B, by = 100)) + 
-#   facet_grid(rows = vars(factor(Group_label, 
-#                                 levels = c("Unimpaired", "MCI", "Dementia", 
-#                                            "Other")))) + theme_bw() 
-# 
-# ggsave(filename = "varY_chain.jpeg", plot = varY_chain_plot, 
-#        path = "/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-#        width = 7, height = 5, units = "in", device = "jpeg")
 
 #---- save datasets ----
 write_csv(gamma_plot_data, 
