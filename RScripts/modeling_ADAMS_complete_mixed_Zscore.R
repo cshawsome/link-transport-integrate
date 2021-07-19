792e7aacb82f5bbccda823d3e8a65a3633984c9a
Crystal Shaw <c.shaw@ucla.edu>
2021-07-18 17:32:50 -0700

    add plot creation and saving dx data to function

--- RScripts/modeling_ADAMS_complete_mixed_Zscore.R
+++ RScripts/modeling_ADAMS_complete_mixed_Zscore.R
@@ -77,11 +77,15 @@ synthetic_sample <- arrange(synthetic_sample,
 #---- **synthetic data function ----
 generate_synthetic <- 
   function(warm_up, synthetic_sets, synthetic_sample, run, starting_props, A){
-    #generate subfolders to store results
+    #---- generate subfolders for results ----
     dir.create(paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
                       "results/ADAMSA/standard_normal/run_", run))
+    dir.create(paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
+                      "standard_normal/run_", run))
+    dir.create(paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                      "ADAMSA/standard_normal/diagnostics_data/run_", run))
     
-    #---- number to sample ----
+    #---- sampling counts ----
     warm_up = warm_up
     synthetic_sets = synthetic_sets
     B = warm_up + synthetic_sets
@@ -114,6 +118,7 @@ generate_synthetic <-
         expand.grid(seq(1, 4), seq(1:nrow(cross_class_label)), seq(1:B)), 1, paste,
         collapse = ":")) %>% set_rownames(Z[, "label"])
     
+    #---- start sampling ----
     for(b in 1:B){
       if(b == 1){
         #---- ****init group membership ----
@@ -298,10 +303,163 @@ generate_synthetic <-
                                 run, "/ADAMSA_synthetic_", b - warm_up, ".csv"))
       }
     }
-    #---- ****return ----
-    return(list("model_gamma_chain" = model_gamma_chain, 
-                "latent_class_chain" = latent_class_chain, "pi_chain" = pi_chain, 
-                "Sigma_chain" = Sigma_chain, "mu_chain" = mu_chain))
+    #---- **dx plots ----
+    #---- ****color palettes ----
+    extended_pallette10 <- colorRampPalette(wes_palette("Darjeeling1"))(10)
+    extended_pallette14 <- colorRampPalette(wes_palette("Darjeeling1"))(14)
+    extended_pallette6 <- colorRampPalette(wes_palette("Darjeeling1"))(6)
+    
+    #---- ****gamma chains ----
+    gamma_plot_data <- model_gamma_chain %>% as.data.frame() %>% 
+      set_colnames(c(seq(1:B), "model", "pred")) %>%
+      pivot_longer(cols = paste0(seq(1:B)), 
+                   names_to = "Run", values_to = "gamma") %>% 
+      filter(pred != "(Intercept)")
+    
+    gamma_chain_plot <- 
+      ggplot(data = gamma_plot_data, 
+             aes(x = reorder(Run, sort(as.numeric(Run))), y = gamma, 
+                 colour = pred)) + geom_line(aes(group = pred)) + 
+      facet_grid(rows = vars(factor(model, 
+                                    levels = c("normal", "mci", "other"))), 
+                 scales = "free") + 
+      geom_vline(xintercept = burn_in, size = 1) + theme_bw() + xlab("Run") + 
+      scale_x_discrete(breaks = seq(0, B, by = 100)) + 
+      scale_color_manual(values = rev(extended_pallette14))
+    
+    ggsave(filename = "gamma_chain.jpeg", plot = gamma_chain_plot, 
+           path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                         "diagnostics/standard_normal/run_", run), 
+           width = 7, height = 6, units = "in", device = "jpeg")
+    
+    #---- **latent class chain ----
+    latent_class_data <- t(latent_class_chain) %>% as.data.frame() %>%
+      mutate("run" = seq(1:B)) %>% 
+      pivot_longer(-c("run"), names_to = c("Group"), values_to = "prob") %>% 
+      arrange(desc(prob)) %>%
+      mutate_at("Group", as.factor)
+    latent_class_data$Group <- fct_relevel(latent_class_data$Group, 
+                                           paste0(unique(latent_class_data$Group)))
+    
+    latent_class_chain_plot <- 
+      ggplot(data = latent_class_data, aes(x = run, y = prob, colour = Group)) +       
+      geom_line(aes(group = Group)) + geom_vline(xintercept = burn_in, size = 1) + 
+      theme_minimal() + xlab("Run") + ylab("Proportion of Sample") +  
+      scale_color_manual(values = c(wes_palette("Darjeeling1")[1], 
+                                    wes_palette("Darjeeling1")[3], 
+                                    wes_palette("Darjeeling1")[2], 
+                                    wes_palette("Darjeeling1")[5])) + 
+      scale_x_continuous(breaks = seq(0, B, by = 100)) 
+    
+    ggsave(filename = "latent_class_chain.jpeg", plot = latent_class_chain_plot, 
+           path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                         "diagnostics/standard_normal/run_", run), 
+           width = 7, height = 3, units = "in", device = "jpeg")
+    
+    #---- **pi chain ----
+    pi_chain_data <- pi_chain %>% as.data.frame() %>% rownames_to_column("Cell") %>% 
+      pivot_longer(-c("Cell"), names_to = c("Group", "Run"), names_sep = ":", 
+                   values_to = "probability") %>% 
+      mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
+                                       Group == 2 ~ "Other", Group == 3 ~ "MCI", 
+                                       Group == 4 ~ "Dementia")) %>% 
+      mutate_at("Run", as.numeric) %>%
+      mutate_if(is.character, as.factor) 
+    
+    pi_chain_plot <- ggplot(data = pi_chain_data, 
+                            aes(x = Run, y = probability, colour = Cell)) +       
+      geom_line(aes(group = Cell)) + geom_vline(xintercept = burn_in, size = 1) + 
+      xlab("Run") + ylab("Probability of cell membership") +  
+      scale_color_manual(values = extended_pallette6) + 
+      scale_x_continuous(breaks = seq(0, B, by = 100)) +
+      facet_grid(rows = vars(factor(Group_label, 
+                                    levels = c("Unimpaired", "MCI", "Dementia", 
+                                               "Other")))) + theme_bw() 
+    
+    ggsave(filename = "pi_chain.jpeg", plot = pi_chain_plot, 
+           path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                         "diagnostics/standard_normal/run_", run), 
+           width = 7, height = 5, units = "in", device = "jpeg")
+    
+    #---- **Sigma chain ----
+    Sigma_chain_data <- Sigma_chain %>% as.data.frame() %>% 
+      rownames_to_column("Z") %>% 
+      pivot_longer(-c("Z"), names_to = c("Group", "Run"), names_sep = ":", 
+                   values_to = "variance") %>% 
+      mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
+                                       Group == 2 ~ "Other", Group == 3 ~ "MCI", 
+                                       Group == 4 ~ "Dementia")) %>%
+      mutate_at("Run", as.numeric) %>%
+      mutate_if(is.character, as.factor) 
+    
+    Sigma_chain_plot <- ggplot(data = Sigma_chain_data, 
+                               aes(x = Run, y = variance, colour = Z)) +       
+      geom_line(aes(group = Z)) + geom_vline(xintercept = burn_in, size = 1) +
+      xlab("Run") + ylab("Variance") +  
+      scale_color_manual(values = rev(extended_pallette10)) + 
+      scale_x_continuous(breaks = seq(0, B, by = 100)) + 
+      facet_grid(rows = vars(factor(Group_label, 
+                                    levels = c("Unimpaired", "MCI", "Dementia", 
+                                               "Other")))) + theme_bw() 
+    
+    ggsave(filename = "Sigma_chain.jpeg", plot = Sigma_chain_plot, 
+           path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                         "diagnostics/standard_normal/run_", run), 
+           width = 7, height = 5, units = "in", device = "jpeg")
+    
+    #---- **mu chain ----
+    mu_chain_data <- mu_chain %>% as.data.frame() %>% 
+      rownames_to_column("Z") %>% 
+      pivot_longer(-c("Z"), names_to = c("Group", "Cell", "Run"), names_sep = ":", 
+                   values_to = "mu") %>% 
+      mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
+                                       Group == 2 ~ "Other", Group == 3 ~ "MCI", 
+                                       Group == 4 ~ "Dementia")) %>% 
+      mutate_at("Run", as.numeric) %>%
+      mutate_if(is.character, as.factor) 
+    
+    mu_chain_plot <- ggplot(data = mu_chain_data, 
+                            aes(x = Run, y = mu, colour = Group_label)) +       
+      geom_line(aes(group = Group_label), alpha = 0.75) + 
+      xlab("Run") + ylab("mu") + geom_vline(xintercept = burn_in, size = 1) + 
+      scale_color_manual(values = c(wes_palette("Darjeeling1")[1], 
+                                    wes_palette("Darjeeling1")[3], 
+                                    wes_palette("Darjeeling1")[5], 
+                                    wes_palette("Darjeeling1")[2])) +
+      scale_x_continuous(breaks = seq(0, B, by = 100)) + 
+      facet_grid(rows = vars(factor(Z)), cols = vars(factor(Cell)), 
+                 scales = "free") + theme_bw() 
+    
+    ggsave(filename = "mu_chain.jpeg", plot = mu_chain_plot, 
+           path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
+                         "diagnostics/standard_normal/run_", run), 
+           width = 14, height = 10, units = "in", device = "jpeg")
+    
+    #---- save datasets ----
+    write_csv(gamma_plot_data, 
+              file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                            "ADAMSA/standard_normal/diagnostics_data/run_", run, 
+                            "ADAMSA_gamma_plot_data.csv"))
+    
+    write_csv(latent_class_data, 
+              file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                            "ADAMSA/standard_normal/diagnostics_data/run_", run,  
+                            "ADAMSA_latent_class_data.csv"))
+    
+    write_csv(pi_chain_data, 
+              file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                            "ADAMSA/standard_normal/diagnostics_data/run_", run,  
+                            "ADAMSA_pi_chain_data.csv"))
+    
+    write_csv(Sigma_chain_data, 
+              file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                            "ADAMSA/standard_normal/diagnostics_data/run_", run,  
+                            "ADAMSA_Sigma_chain_data.csv"))
+    
+    write_csv(mu_chain_data, 
+              file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
+                            "ADAMSA/standard_normal/diagnostics_data/run_", run,  
+                            "ADAMSA_mu_chain_data.csv"))
   }
 
 #---- run function ----
@@ -323,162 +481,7 @@ results <- generate_synthetic(warm_up = 50, synthetic_sets = 100,
                               starting_props = c(0.40, 0.20, 0.10, 0.30), A = A)
 stop <- Sys.time() - start
 
-#---- plots ----
-extended_pallette10 <- colorRampPalette(wes_palette("Darjeeling1"))(10)
-extended_pallette14 <- colorRampPalette(wes_palette("Darjeeling1"))(14)
-extended_pallette6 <- colorRampPalette(wes_palette("Darjeeling1"))(6)
 
-#---- **gamma chains ----
-gamma_plot_data <- results$model_gamma_chain %>% as.data.frame() %>% 
-  set_colnames(c(seq(1:B), "model", "pred")) %>%
-  pivot_longer(cols = paste0(seq(1:B)), 
-               names_to = "Run", values_to = "gamma") %>% 
-  filter(pred != "(Intercept)")
-
-gamma_chain_plot <- 
-  ggplot(data = gamma_plot_data, 
-         aes(x = reorder(Run, sort(as.numeric(Run))), y = gamma, 
-             colour = pred)) + geom_line(aes(group = pred)) + 
-  facet_grid(rows = vars(factor(model, 
-                                levels = c("normal", "mci", "other"))), 
-             scales = "free") + 
-  geom_vline(xintercept = burn_in, size = 1) + theme_bw() + xlab("Run") + 
-  scale_x_discrete(breaks = seq(0, B, by = 100)) + 
-  scale_color_manual(values = rev(extended_pallette14))
-
-ggsave(filename = "gamma_chain.jpeg", plot = gamma_chain_plot, 
-       path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-                     "standard_normal"), 
-       width = 7, height = 6, units = "in", device = "jpeg")
-
-#---- **latent class chain ----
-latent_class_data <- t(latent_class_chain) %>% as.data.frame() %>%
-  mutate("run" = seq(1:B)) %>% 
-  pivot_longer(-c("run"), names_to = c("Group"), values_to = "prob") %>% 
-  arrange(desc(prob)) %>%
-  mutate_at("Group", as.factor)
-latent_class_data$Group <- fct_relevel(latent_class_data$Group, 
-                                       paste0(unique(latent_class_data$Group)))
-
-latent_class_chain_plot <- 
-  ggplot(data = latent_class_data, aes(x = run, y = prob, colour = Group)) +       
-  geom_line(aes(group = Group)) + geom_vline(xintercept = burn_in, size = 1) + 
-  theme_minimal() + xlab("Run") + ylab("Proportion of Sample") +  
-  scale_color_manual(values = c(wes_palette("Darjeeling1")[1], 
-                                wes_palette("Darjeeling1")[3], 
-                                wes_palette("Darjeeling1")[2], 
-                                wes_palette("Darjeeling1")[5])) + 
-  scale_x_continuous(breaks = seq(0, B, by = 100)) 
-
-ggsave(filename = "latent_class_chain.jpeg", plot = latent_class_chain_plot, 
-       path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-                     "standard_normal"), 
-       width = 7, height = 3, units = "in", device = "jpeg")
-
-#---- **pi chain ----
-pi_chain_data <- pi_chain %>% as.data.frame() %>% rownames_to_column("Cell") %>% 
-  pivot_longer(-c("Cell"), names_to = c("Group", "Run"), names_sep = ":", 
-               values_to = "probability") %>% 
-  mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
-                                   Group == 2 ~ "Other", Group == 3 ~ "MCI", 
-                                   Group == 4 ~ "Dementia")) %>% 
-  mutate_at("Run", as.numeric) %>%
-  mutate_if(is.character, as.factor) 
-
-pi_chain_plot <- ggplot(data = pi_chain_data, 
-                        aes(x = Run, y = probability, colour = Cell)) +       
-  geom_line(aes(group = Cell)) + geom_vline(xintercept = burn_in, size = 1) + 
-  xlab("Run") + ylab("Probability of cell membership") +  
-  scale_color_manual(values = extended_pallette6) + 
-  scale_x_continuous(breaks = seq(0, B, by = 100)) +
-  facet_grid(rows = vars(factor(Group_label, 
-                                levels = c("Unimpaired", "MCI", "Dementia", 
-                                           "Other")))) + theme_bw() 
-
-ggsave(filename = "pi_chain.jpeg", plot = pi_chain_plot, 
-       path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-                     "standard_normal"), 
-       width = 7, height = 5, units = "in", device = "jpeg")
-
-#---- **Sigma chain ----
-Sigma_chain_data <- Sigma_chain %>% as.data.frame() %>% 
-  rownames_to_column("Z") %>% 
-  pivot_longer(-c("Z"), names_to = c("Group", "Run"), names_sep = ":", 
-               values_to = "variance") %>% 
-  mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
-                                   Group == 2 ~ "Other", Group == 3 ~ "MCI", 
-                                   Group == 4 ~ "Dementia")) %>%
-  mutate_at("Run", as.numeric) %>%
-  mutate_if(is.character, as.factor) 
-
-Sigma_chain_plot <- ggplot(data = Sigma_chain_data, 
-                           aes(x = Run, y = variance, colour = Z)) +       
-  geom_line(aes(group = Z)) + geom_vline(xintercept = burn_in, size = 1) +
-  xlab("Run") + ylab("Variance") +  
-  scale_color_manual(values = rev(extended_pallette10)) + 
-  scale_x_continuous(breaks = seq(0, B, by = 100)) + 
-  facet_grid(rows = vars(factor(Group_label, 
-                                levels = c("Unimpaired", "MCI", "Dementia", 
-                                           "Other")))) + theme_bw() 
-
-ggsave(filename = "Sigma_chain.jpeg", plot = Sigma_chain_plot, 
-       path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-                     "standard_normal"), 
-       width = 7, height = 5, units = "in", device = "jpeg")
-
-#---- **mu chain ----
-mu_chain_data <- mu_chain %>% as.data.frame() %>% 
-  rownames_to_column("Z") %>% 
-  pivot_longer(-c("Z"), names_to = c("Group", "Cell", "Run"), names_sep = ":", 
-               values_to = "mu") %>% 
-  mutate("Group_label" = case_when(Group == 1 ~ "Unimpaired", 
-                                   Group == 2 ~ "Other", Group == 3 ~ "MCI", 
-                                   Group == 4 ~ "Dementia")) %>% 
-  mutate_at("Run", as.numeric) %>%
-  mutate_if(is.character, as.factor) 
-
-mu_chain_plot <- ggplot(data = mu_chain_data, 
-                        aes(x = Run, y = mu, colour = Group_label)) +       
-  geom_line(aes(group = Group_label), alpha = 0.75) + 
-  xlab("Run") + ylab("mu") + geom_vline(xintercept = burn_in, size = 1) + 
-  scale_color_manual(values = c(wes_palette("Darjeeling1")[1], 
-                                wes_palette("Darjeeling1")[3], 
-                                wes_palette("Darjeeling1")[5], 
-                                wes_palette("Darjeeling1")[2])) +
-  scale_x_continuous(breaks = seq(0, B, by = 100)) + 
-  facet_grid(rows = vars(factor(Z)), cols = vars(factor(Cell)), 
-             scales = "free") + theme_bw() 
-
-ggsave(filename = "mu_chain.jpeg", plot = mu_chain_plot, 
-       path = paste0("/Users/CrystalShaw/Box/Dissertation/figures/diagnostics/", 
-                     "standard_normal"), 
-       width = 14, height = 10, units = "in", device = "jpeg")
-
-#---- save datasets ----
-write_csv(gamma_plot_data, 
-          file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
-                        "ADAMSA/standard_normal/diagnostics_data/", 
-                        "ADAMSA_gamma_plot_data.csv"))
-
-write_csv(latent_class_data, 
-          file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
-                        "ADAMSA/standard_normal/diagnostics_data/", 
-                        "ADAMSA_latent_class_data.csv"))
-
-write_csv(pi_chain_data, 
-          file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
-                        "ADAMSA/standard_normal/diagnostics_data/", 
-                        "ADAMSA_pi_chain_data.csv"))
-
-write_csv(Sigma_chain_data, 
-          file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
-                        "ADAMSA/standard_normal/diagnostics_data/", 
-                        "ADAMSA_Sigma_chain_data.csv"))
-
-write_csv(mu_chain_data, 
-          file = paste0("/Users/CrystalShaw/Box/Dissertation/analyses/results/", 
-                        "ADAMSA/standard_normal/diagnostics_data/", 
-                        "ADAMSA_mu_chain_data.csv"))
 
 
 
