4c5f3e0308f1896b3de14851a6361d8976e8dd88
Crystal Shaw <c.shaw@ucla.edu>
2021-07-28 15:10:10 -0700

    convert analysis for proportions

--- ADAMS/analysis_scripts/ADAMS_test/3b_priors_contingency_cell_counts.R
+++ ADAMS/analysis_scripts/ADAMS_test/3b_priors_contingency_cell_counts.R
@@ -17,28 +17,28 @@ W <- c("Black", "Hispanic", "Astroke")
 #---- cross-class race/ethnicity x stroke ----
 overall_data_counts <- as.data.frame(table(ADAMS_subset$ETHNIC_label, 
                                            ADAMS_subset$Astroke)) %>% 
-  mutate("percent" = round((Freq/sum(Freq))*100, 1))
+  mutate("prop" = Freq/sum(Freq))
 
 for(group in unique(ADAMS_subset$Adem_dx_cat)){
   subset <- ADAMS_subset %>% filter(Adem_dx_cat == group)
   assign(paste0(group, "_data_counts"), 
          as.data.frame(table(subset$ETHNIC_label, 
                              subset$Astroke)) %>% 
-           mutate("percent" = round((Freq/sum(Freq))*100, 1)) %>% 
+           mutate("prop" = Freq/sum(Freq)) %>% 
            unite("cell", c("Var1", "Var2"), sep = ":")) 
 }
 
-#---- bootstrap counts ----
-B = 10000
-bootstrap_counts <- matrix(0, nrow = 6*4, ncol = (B + 2)) %>% 
+#---- bootstrap props ----
+B = 10
+bootstrap_props <- matrix(0, nrow = 6*4, ncol = (B + 2)) %>% 
   as.data.frame() %>% set_colnames(c(seq(1, B), "group", "cell"))
-bootstrap_counts[, "group"] <- rep(unique(ADAMS_subset$Adem_dx_cat), each = 6)
+bootstrap_props[, "group"] <- rep(unique(ADAMS_subset$Adem_dx_cat), each = 6)
 
 cells <- 
   as.data.frame(table(ADAMS_subset$ETHNIC_label, ADAMS_subset$Astroke)) %>% 
   unite("cell", c("Var1", "Var2"), sep = ":")
 
-bootstrap_counts[, "cell"] <- rep(cells$cell, 4)
+bootstrap_props[, "cell"] <- rep(cells$cell, 4)
 
 for(b in 1:B){
   sample <- sample_n(ADAMS_subset, size = nrow(ADAMS_subset), replace = TRUE)
@@ -49,23 +49,24 @@ for(b in 1:B){
       as.data.frame(table(sub_sample$ETHNIC_label, sub_sample$Astroke)) %>% 
       unite("cell", c("Var1", "Var2"), sep = ":")
     
-    bootstrap_counts[
-      which(bootstrap_counts$group == group & 
-              bootstrap_counts$cell %in% counts$cell), b] <- counts$Freq
+    bootstrap_props[
+      which(bootstrap_props$group == group & 
+              bootstrap_props$cell %in% counts$cell), b] <- 
+      counts$Freq/nrow(sub_sample)
   }
 }
 
-bootstrap_count_plot_data <- bootstrap_counts %>% mutate("truth" = 0) 
+bootstrap_props_plot_data <- bootstrap_props %>% mutate("truth" = 0) 
 
 for(group in unique(ADAMS_subset$Adem_dx_cat)){
   true_counts <- get(paste0(group, "_data_counts"))
-  bootstrap_count_plot_data[
-    which(bootstrap_count_plot_data$group == group & 
-            bootstrap_count_plot_data$cell %in% counts$cell), "truth"] <- 
-    true_counts$Freq
+  bootstrap_props_plot_data[
+    which(bootstrap_props_plot_data$group == group & 
+            bootstrap_props_plot_data$cell %in% true_counts$cell), "truth"] <- 
+    true_counts$prop
 }
 
-bootstrap_count_plot_data %<>% 
+bootstrap_props_plot_data %<>% 
   mutate("cat" = rep(c("Black + No Stroke", "Hispanic + No Stroke", 
                        "White + No Stroke", "Black + Stroke", 
                        "Hispanic + Stroke", "White + Stroke"), 4)) %>% 
@@ -78,15 +79,15 @@ bootstrap_count_plot_data %<>%
 #---- **plots ----
 #---- ****create directories ----
 for(dem_group in unique(ADAMS_subset$Adem_dx_cat)){
- dir.create(paste0("/Users/CrystalShaw/Box/Dissertation/figures/ADAMS_train/", 
-                   "prior_predictive_checks/cell_counts/group_specific/", 
+ dir.create(paste0("/Users/CrystalShaw/Box/Dissertation/figures/ADAMS_test/", 
+                   "prior_predictive_checks/cell_props/group_specific/", 
                    tolower(dem_group)), recursive = TRUE) 
 }
 
 #---- ****create plot ----
-for(dem_group in unique(bootstrap_count_plot_data$group)){
-  for(category in unique(bootstrap_count_plot_data$cat)){
-    subset <- bootstrap_count_plot_data %>% 
+for(dem_group in unique(bootstrap_props_plot_data$group)){
+  for(category in unique(bootstrap_props_plot_data$cat)){
+    subset <- bootstrap_props_plot_data %>% 
       filter(group == dem_group & cat == category)
     ggplot(data = subset , aes(x = value)) + 
       geom_histogram(fill = "black", color = "black") + theme_minimal() + 
@@ -95,20 +96,20 @@ for(dem_group in unique(bootstrap_count_plot_data$group)){
                  size = 2)
     
     ggsave(filename = paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
-                             "ADAMS_train/prior_predictive_checks/cell_counts/", 
+                             "ADAMS_test/prior_predictive_checks/cell_props/", 
                              "group_specific/", tolower(dem_group), "/", 
-                             tolower(dem_group), "_", category, "_count.jpeg"), 
+                             tolower(dem_group), "_", category, "_prop.jpeg"), 
            width = 5, height = 3, units = "in")
   } 
 }
 
-bootstrap_counts %>% as.data.frame() %>% 
+bootstrap_props %>% as.data.frame() %>% 
   mutate("group_number" = case_when(group == "Unimpaired" ~ 1, 
                                     group == "Other" ~ 2, 
                                     group == "MCI" ~ 3, 
                                     group == "Dementia" ~ 4)) %>%
-  write_csv(paste0("/Users/CrystalShaw/Box/Dissertation/data/priors/ADAMS_train/", 
-                   "bootstrap_cell_counts.csv"))
+  write_csv(paste0("/Users/CrystalShaw/Box/Dissertation/data/priors/ADAMS_test/", 
+                   "bootstrap_cell_props.csv"))
 
 
 
