cbd8d8fc29cee9d67e12c1c6f0b62830a455a5cf
cshawsome <c.shaw@ucla.edu>
2022-02-02 10:11:17 -0800

    derive bmi and drinks per week vars

--- simulation_studies/A3_ADAMS_variable_selection.R
+++ simulation_studies/A3_ADAMS_variable_selection.R
@@ -11,27 +11,53 @@ ADAMS_imputed <-
   readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets"))
 
 #---- derive post-imputation variables ----
-test <- ADAMS_imputed[[1]]
 hrs_waves <- seq(4, 7)
 
-#---- **bmi ----
-derive_bmi <- function(data, waves){
+derive_vars <- function(data, waves){
   for(wave in waves){
-    data %<>% mutate(!!paste0("r", wave, "bmi_derived") := 
-                       !!sym(paste0("r", wave, "weight"))/
-                       (!!sym(paste0("r", wave, "height"))*
-                          !!sym(paste0("r", wave, "height"))))
+    data %<>% 
+      #---- **bmi ----
+    mutate(!!paste0("r", wave, "bmi_derived") := 
+             !!sym(paste0("r", wave, "weight"))/
+             (!!sym(paste0("r", wave, "height"))*
+                !!sym(paste0("r", wave, "height")))) %>% 
+      #---- **drinks per week ----
+      mutate(!!paste0("r", wave, "drinks_per_week") := 
+               !!sym(paste0("r", wave, "drinkd"))*
+               !!sym(paste0("r", wave, "drinkn")))
   }
   return(data)
 }
 
-ADAMS_imputed <- lapply(ADAMS_imputed, derive_bmi, hrs_waves)
+ADAMS_imputed <- lapply(ADAMS_imputed, derive_vars, hrs_waves)
 
 # #Sanity check
-# test <- ADAMS_imputed[[25]]
+# test <- ADAMS_imputed[[1]]
 # View(test[, c("r4weight", "r4height", "r4bmi_derived")])
 # head(test[, "r4weight"]/(test[, "r4height"]^2))
+# View(test[, c("r4drinkd", "r4drinkn", "r4drinks_per_week")])
 # tail(colnames(test))
+
+#---- representative health variables ----
+
+test %<>% 
+  mutate("drinks_per_week" = Adrinkd*Adrinkn) %>%
+  mutate("DRINKcat" = 
+           case_when(drinks_per_week == 0 ~ 1,
+                     GENDER_label == "Male" &
+                       (drinks_per_week >= 1 & drinks_per_week < 14) ~ 2,
+                     GENDER_label == "Female" &
+                       (drinks_per_week >= 1 & drinks_per_week < 7) ~ 2,
+                     GENDER_label == "Male" &
+                       (drinks_per_week >= 14 | Adrinkn >= 4) ~ 3,
+                     GENDER_label == "Female" &
+                       (drinks_per_week >= 7 | Adrinkn >= 3) ~ 3)) %>% 
+  mutate("DRINKcat_label" = 
+           as.factor(case_when(DRINKcat == 1 ~ "No Drinking", 
+                               DRINKcat == 2 ~ "Moderate Drinking", 
+                               DRINKcat == 3 ~ "Heavy Drinking"))) %>% 
+  mutate(DRINKcat_label = fct_relevel(DRINKcat_label, "No Drinking", 
+                                      "Moderate Drinking"))
 
 #---- define variable types ----
 
