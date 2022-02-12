85f32c7574fddf92a88809c8fc023cbf3bd65a22
cshawsome <c.shaw@ucla.edu>
2022-02-11 18:08:13 -0800

    impute data

--- simulation_studies/B2_HRS_imputation.R
+++ simulation_studies/B2_HRS_imputation.R
@@ -18,51 +18,12 @@ needs_imputing <- names(which(colSums(is.na(HRS_analytic)) != 0))
 needs_imputing <- needs_imputing[-1]
 
 #---- define imputation var types ----
-hrs_waves <- seq(12, 14)
-cog_waves <- seq(11, 13)
-
-sociodem_vars <- c()
-
-ADAMS_vars <- c("SELFCOG", "ANMSETOT", "ANSER7T", "ANSCISOR", "ANCACTUS", 
-                "ANPRES", "ANAFTOT", "ANBNTTOT", "ANDELCOR", "ANRECYES", 
-                "ANRECNO", "ANWM1TOT", "ANWM2TOT", "ANCPTOT", "ANRCPTOT", 
-                "ANTMASEC", "ANBWC20", "ANBWC86", "ANIMMCR", "ANSMEM2_Better", 
-                "ANSMEM2_Same", "ANSMEM2_Worse", "avg_proxy_cog_Better", 
-                "avg_proxy_cog_Same", "avg_proxy_cog_Worse", "Dementia", "Other", 
-                "MCI", "proxy_Spouse", "proxy_Child", "proxy_Other_Relative", 
-                "proxy_Other")
-
-HRS_vars <- c(paste0("r", cog_waves, "mpart"), paste0("r", hrs_waves, "height"), 
-              paste0("r", hrs_waves, "weight"), paste0("r", hrs_waves, "smoken"), 
-              paste0("r", hrs_waves, "drinkd"), paste0("r", hrs_waves, "drinkn"), 
-              paste0("r", hrs_waves, "hibpe"), paste0("r", hrs_waves, "diabe"), 
-              paste0("r", hrs_waves, "hearte"), paste0("r", hrs_waves, "stroke"), 
-              paste0("r", hrs_waves, "adla"), paste0("r", hrs_waves, "iadla"), 
-              paste0("r", cog_waves, "imrc"), paste0("r", cog_waves, "dlrc"), 
-              paste0("r", cog_waves, "ser7"), paste0("r", cog_waves, "bwc20"), 
-              paste0("r", seq(5, 6), "bwc86"), paste0("r", cog_waves, "scis"), 
-              paste0("r", cog_waves, "cact"), paste0("r", cog_waves, "pres"), 
-              paste0("r", cog_waves, "cogtot"), 
-              paste0("r", cog_waves, "pstmem_Better"), 
-              paste0("r", cog_waves, "pstmem_Same"), 
-              paste0("r", cog_waves, "pstmem_Worse"))
-
-not_predictors <- c("HHIDPN", "AYEAR", "White", "ANMSETOT_norm", 
-                    "Adem_dx_label_collapsed", "Unimpaired", "Astroke", "Ahibpe", 
-                    "Adiabe", "Ahearte", "Abmi", "Aiadla", "Aadla", "Asmoken", 
-                    "Adrinkd", "Adrinkn")
-
-# #Sanity check
-# union_var_names <- c(sociodem_vars, ADAMS_vars, HRS_vars, not_predictors)
-# ncol(ADAMS_analytic) == length(union_var_names)
-# 
-# colnames(ADAMS_analytic)[which(!colnames(ADAMS_analytic) %in% union_var_names)]
-# union_var_names[which(!union_var_names %in% colnames(ADAMS_analytic))]
+not_predictors <- c("HHIDPN", "HCAP_SELECT")
 
 #---- predictor matrix ----
 predict <- 
-  matrix(1, nrow = length(needs_imputing), ncol = ncol(ADAMS_analytic)) %>% 
-  set_rownames(needs_imputing) %>% set_colnames(colnames(ADAMS_analytic))
+  matrix(1, nrow = length(needs_imputing), ncol = ncol(HRS_analytic)) %>% 
+  set_rownames(needs_imputing) %>% set_colnames(colnames(HRS_analytic))
 
 #---- **cannot predict themselves ----
 predict[needs_imputing, needs_imputing] <- 
@@ -72,48 +33,16 @@ predict[needs_imputing, needs_imputing] <-
 #---- **non-predictors ----
 predict[, not_predictors] <- 0
 
-#---- **ADAMS-ADAMS prediction ----
-#use ADAMS variables to predict each other: remove HRS predictors for now
-predict[needs_imputing[which(needs_imputing %in% ADAMS_vars)], HRS_vars] <- 0
-
-#---- **HRS-HRS prediction ----
-#use HRS variables to predict each other: remove ADAMS predictors 
-predict[needs_imputing[which(needs_imputing %in% HRS_vars)], ADAMS_vars] <- 0
-
-#---- **ADAMS extra predictors ----
-predict["ANBWC20", paste0("r", cog_waves, "bwc20")] <- 1
-predict["ANBWC86", paste0("r", seq(5, 6), "bwc86")] <- 1
-predict["ANCACTUS", paste0("r", cog_waves, "cact")] <- 1
-predict["SELFCOG", paste0("r", cog_waves, "cogtot")] <- 1
-predict["ANDELCOR", paste0("r", cog_waves, "dlrc")] <- 1
-predict["ANIMMCR", paste0("r", cog_waves, "imrc")] <- 1
-predict["ANPRES", paste0("r", cog_waves, "pres")] <- 1
-predict["ANSCISOR", paste0("r", cog_waves, "scis")] <- 1
-predict["ANSER7T", paste0("r", cog_waves, "ser7")] <- 1
-predict[paste0("ANSMEM2_", c("Better", "Same", "Worse")), 
-        paste0("r", cog_waves, "pstmem_Better")] <- 1
-predict[paste0("ANSMEM2_", c("Better", "Same", "Worse")), 
-        paste0("r", cog_waves, "pstmem_Same")] <- 1
-predict[paste0("ANSMEM2_", c("Better", "Same", "Worse")), 
-        paste0("r", cog_waves, "pstmem_Worse")] <- 1
-
-#---- **derived vars (do not impute) ----
-remove <- c("ANMSETOT_norm", 
-            paste0("A", c("stroke", "hibpe", "diabe", "hearte", "bmi", "iadla", 
-                          "adla", "smoken", "drinkd", "drinkn"))) 
-
-predict <- predict[!rownames(predict) %in% remove, ]
-
 #---- **sanity check ----
 #non-predictors should all be 0
 colSums(predict[, not_predictors])
 
 #---- imputation ----
-set.seed(20220202)
+set.seed(20220211)
 start <- Sys.time()
-fast_impute(predictor_matrix = predict, data = ADAMS_analytic, 
-            path_for_output = paste0(path_to_box, "data/ADAMS/cleaned/"),
-            method = "PMM", m = 25, maxit = 15)
+fast_impute(predictor_matrix = predict, data = HRS_analytic, 
+            path_for_output = paste0(path_to_box, "data/HRS/cleaned/"),
+            method = "PMM", m = 75, maxit = 15)
 end <- Sys.time() - start
 
 #---- read in results ----
