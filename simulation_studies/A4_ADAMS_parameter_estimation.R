3e0727fc0f39c85f4316401c9808f32f08d46025
cshawsome <c.shaw@ucla.edu>
2022-02-13 17:30:51 -0800

    read in selected vars from lasso models

--- simulation_studies/A4_ADAMS_parameter_estimation.R
+++ simulation_studies/A4_ADAMS_parameter_estimation.R
@@ -10,6 +10,12 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 ADAMS_imputed_clean <- 
   readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets_cleaned")) 
 
+#---- read in variable selection results ----
+selected_vars <- 
+  read_csv(paste0(path_to_box, "analyses/simulation_study/variable_selection/", 
+                  "model_coefficients.csv")) %>% 
+  dplyr::select("Variable") %>% unlist()
+
 #---- cell IDs ----
 #Merge Black, Hispanic, and Stroke (ever/never)
 # Ex: 001 is a white participant with a history of stroke
@@ -32,3 +38,10 @@ ADAMS_imputed_clean %<>%
 # head(ADAMS_imputed_clean[[1]]$cell_ID)
 # colnames(ADAMS_imputed_clean[[25]])
 # head(ADAMS_imputed_clean[[25]]$cell_ID)
+
+#---- parameter estimation ----
+continuous_vars <- c("AAGE", "EDYRS", "ANMSETOT_norm", "ANBNTTOT", "ANIMMCR", 
+                     "ANDELCOR", "ANSER7T", "ANAFTOT", "")
+
+#---- **Normal distribution ----
+
