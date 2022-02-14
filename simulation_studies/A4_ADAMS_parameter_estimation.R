001aeffb7b4a77653be5acc6abe9bb91e84db81a
cshawsome <c.shaw@ucla.edu>
2022-02-13 17:02:37 -0800

    read in data

--- simulation_studies/A4_ADAMS_parameter_estimation.R
+++ simulation_studies/A4_ADAMS_parameter_estimation.R
@@ -4,3 +4,18 @@ if (!require("pacman")){
 }
 
 p_load("tidyverse")
+
+#---- read in data ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+ADAMS_imputed_clean <- 
+  readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets_cleaned")) %>%
+  lapply(function(x) mutate_at(x, "HHIDPN", as.numeric))
+
+#---- 
+
+#---- label contingency cells ----
+sample <- ADAMS_imputed_clean[[1]]
+cells <- as.data.frame(table(sample$Black, sample$Hispanic, sample$Astroke)) %>%
+  set_colnames(c("Black", "Hispanic", "Stroke", "Count")) %>% 
+  filter(!(Black == 1 & Hispanic == 1)) %>% 
+  unite("cell_ID", c("Black", "Hispanic", "Stroke"), sep = "")
