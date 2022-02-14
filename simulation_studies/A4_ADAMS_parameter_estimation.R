1547f88994e6e56785faafc1e063ce152ea87b8e
cshawsome <c.shaw@ucla.edu>
2022-02-13 17:16:41 -0800

    cell IDs to all imputed datasets

--- simulation_studies/A4_ADAMS_parameter_estimation.R
+++ simulation_studies/A4_ADAMS_parameter_estimation.R
@@ -8,8 +8,7 @@ p_load("tidyverse")
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 ADAMS_imputed_clean <- 
-  readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets_cleaned")) %>%
-  lapply(function(x) mutate_at(x, "HHIDPN", as.numeric))
+  readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets_cleaned")) 
 
 #---- cell IDs ----
 #Merge Black, Hispanic, and Stroke (ever/never)
@@ -22,4 +21,14 @@ cell_IDs <-
   unite("cell_ID", c("Black", "Hispanic", "Stroke"), sep = "") %>% 
   dplyr::select("cell_ID") %>% unlist()
 
+#add column to all MI datasets
+ADAMS_imputed_clean %<>% 
+  lapply(., function(x) x %<>% 
+           unite("cell_ID", c("Black", "Hispanic", "Astroke"), sep = "", 
+                 remove = FALSE))
 
+# #Sanity check
+# colnames(ADAMS_imputed_clean[[1]])
+# head(ADAMS_imputed_clean[[1]]$cell_ID)
+# colnames(ADAMS_imputed_clean[[25]])
+# head(ADAMS_imputed_clean[[25]]$cell_ID)
