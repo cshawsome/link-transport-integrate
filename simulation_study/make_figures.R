221ff6a4c4e96ad1bc09c3845c865eec3b462d53
cshawsome <c.shaw@ucla.edu>
2022-03-03 15:08:40 -0800

    stack and rename ADAMS imputed datasets

--- simulation_study/make_figures.R
+++ simulation_study/make_figures.R
@@ -6,7 +6,13 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 ADAMS_imputed_clean <- 
   readRDS(paste0(path_to_box, "data/ADAMS/cleaned/MI/MI_datasets_cleaned"))
 
+#stack data and rename variables
+ADAMS_imputed_stacked <- do.call(rbind, ADAMS_imputed_clean) %>% 
+  rename_at(vars(variable_labels$ADAMS[-1]), ~ variable_labels$data_label[-1])
+
 #---- ****synthetic HRS ----
 synthetic_normal_1000 <- 
   read_csv(paste0(path_to_box, "analyses/simulation_study/", "synthetic_data/", 
                   "synthetic_normal_1000.csv"))
+
+
