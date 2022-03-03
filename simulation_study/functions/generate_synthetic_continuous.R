e9768510c2f4acdc7575f6b3c6ee1b58ce17b387
cshawsome <c.shaw@ucla.edu>
2022-03-03 10:44:57 -0800

    perform check on impairment proportions

--- simulation_study/functions/generate_synthetic_continuous.R
+++ simulation_study/functions/generate_synthetic_continuous.R
@@ -1,12 +1,31 @@
-generate_synthetic_continuous <- function(data, sample_size, dist, parameters){
-  #--- bootstrap data ----
-  synthetic_data <- sample_n(data, size = sample_size, replace = TRUE)
-  
-  return(synthetic_data)
-}
+generate_synthetic_continuous <- 
+  function(data, sample_size, normal_prop = 0.25, mci_prop = 0.25, 
+           dementia_prop = 0.25, dist, parameters){
+    #---- check parameters ----
+    if(sum(normal_prop, mci_prop, dementia_prop) > 1){
+      stop("Impairment proportions sum to more than 1. Please check values.") 
+    }
+    
+    #--- bootstrap data ----
+    synthetic_data <- sample_n(data, size = sample_size, replace = TRUE)
+    
+    #---- assign impairment category ----
+    
+    return(synthetic_data)
+  }
 
 #Testing
 data <- HRS_analytic
 sample_size <- 2000
+normal_prop = 0.5
+mci_prop = 0.3
+dementia_prop = 0.3
 dist <- "normal"
 parameters <- normal_parameter_list
+
+test <- 
+  generate_synthetic_continuous(data, sample_size, normal_prop = 0.3, 
+                                mci_prop = 0.3, dementia_prop = 0.3, dist, 
+                                parameters)
+
+
