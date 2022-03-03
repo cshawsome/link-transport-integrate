35f0903dea4e7ce33f27d034ca9205bcdddad0c0
cshawsome <c.shaw@ucla.edu>
2022-03-03 15:08:11 -0800

    save synthetic normal data

--- simulation_study/B2_HRS_synthetic.R
+++ simulation_study/B2_HRS_synthetic.R
@@ -33,4 +33,13 @@ HRS_analytic %<>% mutate("Intercept" = 1) %>%
 #---- synthetic data ----
 set.seed(20220303)
 #---- **multivariate normal ----
+synthetic_normal_1000 <- 
+  generate_synthetic_continuous(HRS_analytic, sample_size = 1000, 
+                                unimpaired_prop = 0.35, mci_prop = 0.10, 
+                                dementia_prop = 0.35, dist = "normal", 
+                                parameters = normal_parameter_list)
+
+write_csv(as.data.frame(synthetic_normal_1000), 
+          paste0(path_to_box, "analyses/simulation_study/", "synthetic_data/", 
+                 "synthetic_normal_1000.csv"))
 
