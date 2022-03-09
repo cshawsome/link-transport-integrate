b709388b15b7c351c551edee8aac707f845709a7
cshawsome <c.shaw@ucla.edu>
2022-03-09 11:18:43 -0800

    Initial commit

---
+++ simulation_study/C_HCAP_synthetic.R
@@ -0,0 +1,9 @@
+#---- Package loading ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("here", "tidyverse")
+
+#---- read in data ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
