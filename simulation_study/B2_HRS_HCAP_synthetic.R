51e4e12cdac5b3a7266a2aef5f0d618a6a96bc64
cshawsome <c.shaw@ucla.edu>
2022-03-10 10:20:58 -0800

    rename cuz generating two sets of synthetic data at once

--- simulation_study/B2_HRS_HCAP_synthetic.R
+++ simulation_study/B2_HRS_HCAP_synthetic.R
@@ -3,7 +3,7 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("here", "tidyverse", "LaplacesDemon", "MBSP")
+p_load("here", "tidyverse", "magrittr", "LaplacesDemon", "MBSP")
 
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
