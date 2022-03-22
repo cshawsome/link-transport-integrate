44f0bce71382de06b79fdc003b33ee3a3a809850
cshawsome <c.shaw@ucla.edu>
2022-03-22 12:12:02 -0700

    no change

--- simulation_study/functions/prior_predictive_checks_function.R
+++ simulation_study/functions/prior_predictive_checks_function.R
@@ -1,6 +1,6 @@
 prior_predictive_checks <- 
   function(unimpaired_preds, other_preds, mci_preds, categorical_vars, 
-           continuous_vars, id_var, dementia_var, dataset_to_copy, 
+           continuous_vars, id_var, dementia_var, dataset_to_copy,
            num_synthetic, unimpaired_betas, unimpaired_cov, other_betas, 
            other_cov, mci_betas, mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
            prior_beta, nu_0, kappa_0, contrasts_matrix, path_to_folder){
