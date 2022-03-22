94b84cadbbf662f05dae13786ab39c48d2222ab2
cshawsome <c.shaw@ucla.edu>
2022-03-22 11:17:45 -0700

    get rid of old code that's already been refactored

--- simulation_study/4_analysis.R
+++ simulation_study/4_analysis.R
@@ -93,89 +93,9 @@ prior_predictive_checks(unimpaired_preds, other_preds, mci_preds,
 
 #---- source functions ----
 source(here::here("ADAMS", "analysis_scripts", "functions", 
-                  "ADAMS_prior_predictive_checks_props_function.R"))
-source(here::here("ADAMS", "analysis_scripts", "functions", 
                   "generate_synthetic_function.R"))
 source(here::here("ADAMS", "analysis_scripts", "functions", 
                   "ADAMS_posterior_predictive_checks.R"))
-
-#---- read in data ----
-
-#complete dataset
-ADAMS_data <- vroom(paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                           "data/ADAMS/cleaned/ADAMS_subset_mixed.csv"))
-
-
-#---- prior predictive checks ----
-#---- **specifying priors ----
-#---- ****latent classes ----
-#based on analysis in priors_latent_classes.R
-for(group in c("unimpaired", "mci", "other")){
-  assign(paste0(group, "_betas"), 
-         vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/priors/", 
-                      "ADAMS_test/latent_class_", group, "_betas.csv")))
-  assign(paste0(group, "_cov"), 
-         vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/priors/", 
-                      "ADAMS_test/latent_class_", group, "_cov.csv")))
-  
-  assign(paste0(group, "_preds"), get(paste0(group, "_betas"))$preds)
-}
-
-#---- ****contingency cells ----
-alpha_0_dist <- 
-  vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/priors/", 
-               "ADAMS_test/bootstrap_cell_props.csv")) 
-
-#--- ****beta and sigma ----
-priors_beta <- vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/",
-                            "priors/ADAMS_test/priors_beta.csv")) 
-prior_V_inv <- vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
-                            "priors/ADAMS_test/priors_V_inv.csv")) 
-prior_Sigma <- vroom(paste0("/Users/CrystalShaw/Box/Dissertation/data/", 
-                            "priors/ADAMS_test/priors_Sigma.csv")) 
-
-#---- **contrasts matrix ----
-A = do.call(cbind, list(
-  #intercept
-  rep(1, 6),
-  #race/ethnicity main effect: Black
-  rep(c(1, 0, 0), 2),
-  #race/ethnicity main effect: Hispanic
-  rep(c(0, 1, 0), 2),
-  #stroke main effect
-  rep(c(0, 1), each = 3)))
-
-#---- **hyperparameters (tune these) ----
-#DOF for inverse wishart
-nu_0 <- 65
-#scaling for inverse wishart as variance of Beta
-kappa_0 <- c(0.85, 0.85, 0.85, 0.85)
-
-#---- **original means and variances ----
-ADAMS_data %<>% 
-  dplyr::select("HHIDPN", all_of(W), all_of(Z[, "var"]), "Adem_dx_cat") %>% 
-  filter(HHIDPN %in% dataset_to_copy$HHIDPN)
-
-ADAMS_means <- colMeans(ADAMS_data %>% dplyr::select(all_of(Z[, "var"])))
-ADAMS_sds <- apply(ADAMS_data %>% dplyr::select(all_of(Z[, "var"])), 2, sd)
-
-#---- **run checks ----
-ADAMS_prior_predictive_checks_props(unimpaired_preds, other_preds, mci_preds, 
-                                    categorical_vars = W, continuous_vars = Z, 
-                                    id_var = "HHIDPN", 
-                                    dementia_var = "Adem_dx_cat", 
-                                    dataset_to_copy, num_synthetic = 1000, 
-                                    unimpaired_betas, unimpaired_cov, 
-                                    other_betas, other_cov, mci_betas, mci_cov, 
-                                    alpha_0_dist, prior_Sigma, prior_V_inv, 
-                                    prior_beta, nu_0, kappa_0, 
-                                    contrasts_matrix = A, 
-                                    orig_means = ADAMS_means, 
-                                    orig_sds = ADAMS_sds, 
-                                    path_to_folder = 
-                                      paste0("/Users/CrystalShaw/Box/",
-                                             "Dissertation/figures/ADAMS_test/", 
-                                             "prior_predictive_checks/"))
 
 #---- synthetic datasets ----
 #starting_props are for (normal, other, mci, dementia)
