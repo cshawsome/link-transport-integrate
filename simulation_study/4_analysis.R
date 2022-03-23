0be440e2d943e5bd34efe407e6ae8b86f678c6f9
cshawsome <c.shaw@ucla.edu>
2022-03-23 16:17:53 -0700

    get rid of old data generating code

--- simulation_study/4_analysis.R
+++ simulation_study/4_analysis.R
@@ -102,95 +102,24 @@ prior_predictive_checks(unimpaired_preds, other_preds, mci_preds,
 generate_synthetic(warm_up = 2, run_number = 1, 
                    starting_props = c(0.25, 0.25, 0.25, 0.25),
                    unimpaired_preds, other_preds, mci_preds, 
-                   categorical_vars = W, continuous_vars = Z, 
-                   id_var = "HHIDPN", dementia_var = "Adem_dx_cat", 
-                   dataset_to_copy, num_synthetic = 10, 
-                   unimpaired_betas, unimpaired_cov, other_betas, 
-                   other_cov, mci_betas, mci_cov, alpha_0_dist, 
+                   categorical_vars = W, continuous_vars = Z, id_var = "HHIDPN", 
+                   variable_labels, dataset_to_copy, cell_ID_key, color_palette,
+                   num_synthetic = 10, unimpaired_betas, unimpaired_cov, 
+                   other_betas, other_cov, mci_betas, mci_cov, alpha_0_dist, 
                    prior_Sigma, prior_V_inv, prior_beta, nu_0, kappa_0, 
                    contrasts_matrix = A,
                    path_to_analyses_folder = 
-                     paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                            "analyses/", "ADAMS_test/"), 
+                     paste0(path_to_box, "analyses/simulation_study/", 
+                            "HCAP_normal_250_unimpaired/"), 
                    path_to_figures_folder = 
-                     paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
-                            "ADAMS_test/"))
+                     paste0(path_to_box, "figures/simulation_study/", 
+                            "HCAP_normal_250_unimpaired/"))
 
 #---- OLD ----
 
 #---- source functions ----
 source(here::here("ADAMS", "analysis_scripts", "functions", 
                   "ADAMS_posterior_predictive_checks.R"))
-
-#---- synthetic datasets ----
-#starting_props are for (normal, other, mci, dementia)
-generate_synthetic_props(warm_up = 2, run_number = 1, 
-                         #warm start
-                         starting_props = c(0.40, 0.13, 0.17, 0.30), 
-                         unimpaired_preds, other_preds, mci_preds, 
-                         categorical_vars = W, continuous_vars = Z, 
-                         id_var = "HHIDPN", dementia_var = "Adem_dx_cat", 
-                         dataset_to_copy, num_synthetic = 10, 
-                         unimpaired_betas, unimpaired_cov, other_betas, 
-                         other_cov, mci_betas, mci_cov, alpha_0_dist, 
-                         prior_Sigma, prior_V_inv, prior_beta, nu_0, kappa_0, 
-                         contrasts_matrix = A,
-                         path_to_analyses_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                                  "analyses/ADAMS_test/"), 
-                         path_to_figures_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/", 
-                                  "figures/ADAMS_test/"))
-
-
-
-generate_synthetic_props(warm_up = 500, run_number = 3, 
-                         starting_props = c(0.10, 0.20, 0.30, 0.40),
-                         unimpaired_preds, other_preds, mci_preds, 
-                         categorical_vars = W, continuous_vars = Z, 
-                         id_var = "HHIDPN", dementia_var = "Adem_dx_cat", 
-                         dataset_to_copy, num_synthetic = 1000, unimpaired_betas, 
-                         unimpaired_cov, other_betas, other_cov, mci_betas, 
-                         mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
-                         prior_beta, nu_0, kappa_0, contrasts_matrix = A,
-                         path_to_analyses_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
-                                  "ADAMS_test/"), 
-                         path_to_figures_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
-                                  "ADAMS_test/"))
-
-generate_synthetic_props(warm_up = 500, run_number = 4, 
-                         starting_props = c(0.10, 0.30, 0.40, 0.20),
-                         unimpaired_preds, other_preds, mci_preds, 
-                         categorical_vars = W, continuous_vars = Z, 
-                         id_var = "HHIDPN", dementia_var = "Adem_dx_cat", 
-                         dataset_to_copy, num_synthetic = 1000, unimpaired_betas, 
-                         unimpaired_cov, other_betas, other_cov, mci_betas, 
-                         mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
-                         prior_beta, nu_0, kappa_0, contrasts_matrix = A,
-                         path_to_analyses_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
-                                  "ADAMS_test/"), 
-                         path_to_figures_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
-                                  "ADAMS_test/"))
-
-generate_synthetic_props(warm_up = 500, run_number = 5, 
-                         starting_props = c(0.05, 0.15, 0.25, 0.55),
-                         unimpaired_preds, other_preds, mci_preds, 
-                         categorical_vars = W, continuous_vars = Z, 
-                         id_var = "HHIDPN", dementia_var = "Adem_dx_cat", 
-                         dataset_to_copy, num_synthetic = 1000, unimpaired_betas, 
-                         unimpaired_cov, other_betas, other_cov, mci_betas, 
-                         mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
-                         prior_beta, nu_0, kappa_0, contrasts_matrix = A,
-                         path_to_analyses_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/analyses/", 
-                                  "ADAMS_test/"), 
-                         path_to_figures_folder = 
-                           paste0("/Users/CrystalShaw/Box/Dissertation/figures/", 
-                                  "ADAMS_test/"))
 
 #---- posterior predictive checks ----
 ADAMS_posterior_predictive_checks(dataset_to_copy, continuous_covariates = Z, 
