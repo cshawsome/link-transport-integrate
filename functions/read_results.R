a7d04108bf17015d74a3b50468f5294dab7cd026
cshawsome <c.shaw@ucla.edu>
2022-03-25 14:49:50 -0700

    add column of dataset names when data is read in

--- functions/read_results.R
+++ functions/read_results.R
@@ -1,3 +1,5 @@
 read_results <- function(path){
-  data.table::fread(path, fill = TRUE) %>% mutate("dataset" = )
+  dataset_name <- str_split(path, pattern = "HRS_synthetic_")[[1]][2]
+  dataset_name <- str_remove(dataset_name, pattern = ".csv")
+  data.table::fread(path, fill = TRUE) %>% mutate("dataset_name" = dataset_name)
 }
--- simulation_study/2c_HRS_simulation_truth.R
+++ simulation_study/2c_HRS_simulation_truth.R
@@ -6,21 +6,17 @@ if (!require("pacman")){
 p_load("here", "tidyverse", "broom")
 
 #---- source scripts ----
-
+source(here::here("functions", "read_results.R"))
 
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
 synthetic_data_paths <- 
   list.files(path = paste0(path_to_box, 
-                           "analyses/simulation_study/synthetic_data/HRS/"), 
+                           "analyses/simulation_study/synthetic_data/HRS"), 
              full.names = TRUE, pattern = "*.csv")
 
 synthetic_data <- do.call(rbind, lapply(synthetic_data_paths, read_results))
-  
-
-read_csv(paste0(path_to_box, "analyses/simulation_study/synthetic_data/HRS/", 
-                  "HRS_synthetic_normal_500_unimpaired.csv"))
 
 #---- analytic models ----
 truth <- glm(Dementia ~ age_Z + female + black + hispanic, 
