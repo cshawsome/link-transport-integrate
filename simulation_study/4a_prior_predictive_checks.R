96e5edaa8df3073563de81865d1cb5455f2bae3b
cshawsome <c.shaw@ucla.edu>
2022-03-26 14:33:14 -0700

    remove extra stuff copied over from analysis function

--- simulation_study/4a_prior_predictive_checks.R
+++ simulation_study/4a_prior_predictive_checks.R
@@ -13,12 +13,6 @@ library(gganimate)
 source(here::here("simulation_study", "functions", 
                   "prior_predictive_checks_function.R"))
 
-source(here::here("simulation_study", "functions", 
-                  "generate_synthetic_function.R"))
-
-source(here::here("simulation_study", "functions", 
-                  "posterior_predictive_checks_function.R"))
-
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
@@ -32,11 +26,7 @@ dataset_to_copy <-
 #---- **variable labels ----
 variable_labels <- read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) 
 
-#---- **cell ID key ----
-cell_ID_key <- read_csv(paste0(path_to_box, "data/cell_ID_key.csv")) %>% 
-  mutate_all(as.character)
-
-#---- **impairement class color palette ----
+#---- **impairment class color palette ----
 color_palette <- read_csv(here::here("color_palette.csv")) 
 
 #---- define vars ----
@@ -87,45 +77,13 @@ kappa_0 <- c(0.85, 0.85, 0.85, 0.85) %>%
 #---- **run checks ----
 prior_predictive_checks(unimpaired_preds, other_preds, mci_preds, 
                         categorical_vars = W, continuous_vars = Z, 
-                        id_var = "HHIDPN", variable_labels = variable_labels,
-                        dementia_var = "Adem_dx_cat", 
-                        dataset_to_copy, num_synthetic = 10, 
-                        unimpaired_betas, unimpaired_cov, 
-                        other_betas, other_cov, mci_betas, mci_cov, 
-                        alpha_0_dist, prior_Sigma, prior_V_inv, 
-                        prior_beta, nu_0, kappa_0, 
-                        contrasts_matrix = A, 
+                        id_var = "HHIDPN", variable_labels, color_palette, 
+                        dataset_to_copy, num_synthetic = 10, unimpaired_betas, 
+                        unimpaired_cov, other_betas, other_cov, mci_betas, 
+                        mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
+                        prior_beta, nu_0, kappa_0, contrasts_matrix = A, 
                         path_to_folder = 
                           paste0(path_to_box,
                                  "figures/simulation_study/", 
                                  "HCAP_normal_250_unimpaired/", 
                                  "prior_predictive_checks/"))
-
-#---- generate synthetic data ----
-generate_synthetic(warm_up = 2, run_number = 1, 
-                   starting_props = c(0.25, 0.25, 0.25, 0.25),
-                   unimpaired_preds, other_preds, mci_preds, 
-                   categorical_vars = W, continuous_vars = Z, id_var = "HHIDPN", 
-                   variable_labels, dataset_to_copy, cell_ID_key, color_palette,
-                   num_synthetic = 10, unimpaired_betas, unimpaired_cov, 
-                   other_betas, other_cov, mci_betas, mci_cov, alpha_0_dist, 
-                   prior_Sigma, prior_V_inv, prior_beta, nu_0, kappa_0, 
-                   contrasts_matrix = A,
-                   path_to_analyses_folder = 
-                     paste0(path_to_box, "analyses/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"), 
-                   path_to_figures_folder = 
-                     paste0(path_to_box, "figures/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"))
-
-#---- posterior predictive checks ----
-posterior_predictive_checks(dataset_to_copy, categorical_covariates = W, 
-                            continuous_covariates = Z, contrasts_matrix = A,
-                            cell_ID_key, variable_labels, num_samples = 10, 
-                            num_chains = 1, color_palette,
-                            path_to_analyses_folder = 
-                              paste0(path_to_box, "analyses/simulation_study/", 
-                                     "HCAP_normal_250_unimpaired/"), 
-                            path_to_figures_folder = 
-                              paste0(path_to_box, "figures/simulation_study/", 
-                                     "HCAP_normal_250_unimpaired/"))
