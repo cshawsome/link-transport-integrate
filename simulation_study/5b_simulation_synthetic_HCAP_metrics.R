6391f67029d145cea0db74486ea24bda1f57fa38
cshawsome <c.shaw@ucla.edu>
2022-03-26 23:23:01 -0700

    read in all simulation data

--- simulation_study/5b_simulation_synthetic_HCAP_metrics.R
+++ simulation_study/5b_simulation_synthetic_HCAP_metrics.R
@@ -5,3 +5,37 @@ if (!require("pacman")){
 
 p_load("tidyverse")
 
+#---- source scripts ----
+source(here::here("functions", "read_results.R"))
+
+#---- read in data ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+
+simulated_data_directories <- 
+  list.dirs(path = paste0(path_to_box, 
+                          "analyses/simulation_study/HCAP_normal_250_unimpaired/", 
+                          "synthetic_data"), full.names = TRUE, 
+            recursive = FALSE)
+
+simulated_data_directories <- 
+  simulated_data_directories[str_detect(simulated_data_directories, 
+                                        pattern = "sim_")]
+
+for(path in simulated_data_directories){
+  simulated_data_paths <- 
+    list.files(path = path, full.names = TRUE, pattern = "*.csv")
+  
+  if(!exists("simulated_data")){
+    simulated_data <- do.call(rbind, lapply(simulated_data_paths, read_results))
+  } else{
+    simulated_data %<>% 
+      rbind(., do.call(rbind, lapply(simulated_data_paths, read_results)))
+  }
+}
+
+#---- impairment class coverage ----
+#---- **true counts ----
+test <- 
+
+
+
