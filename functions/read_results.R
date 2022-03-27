024c3f129bcbd4a4bf9a99b9dd49996c0a4c84e4
cshawsome <c.shaw@ucla.edu>
2022-03-26 22:58:37 -0700

    distinguish between results to be read in

--- functions/read_results.R
+++ functions/read_results.R
@@ -1,5 +1,15 @@
 read_results <- function(path){
-  dataset_name <- str_split(path, pattern = "HRS_synthetic_")[[1]][2]
-  dataset_name <- str_remove(dataset_name, pattern = ".csv")
+  #---- truth table results ----
+  if(str_detect(path, "HRS_synthetic")){
+    dataset_name <- str_split(path, pattern = "HRS_synthetic_")[[1]][2]
+    dataset_name <- str_remove(dataset_name, pattern = ".csv")
+  }
+  
+  #---- simulation study results ----
+  if(str_detect(path, "HCAP_")){
+    dataset_name <- str_split(path, pattern = "HCAP_")[[1]][2]
+    dataset_name <- str_split(dataset_name, pattern = "/")[[1]][1]
+  }
+  
   data.table::fread(path, fill = TRUE) %>% mutate("dataset_name" = dataset_name)
 }
