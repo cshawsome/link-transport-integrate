0a1eee5af2ed38cae896e8e06950d41cbc4d33d0
cshawsome <c.shaw@ucla.edu>
2022-03-26 22:24:53 -0700

    generate simulation study data

--- simulation_study/5_simulation_study.R
+++ simulation_study/5_simulation_study.R
@@ -75,18 +75,23 @@ kappa_0 <- c(0.85, 0.85, 0.85, 0.85) %>%
   set_names(c("Unimpaired", "MCI", "Dementia", "Other"))
 
 #---- generate synthetic data ----
-generate_synthetic(warm_up = 2, run_number = 1, 
-                   starting_props = c(0.25, 0.25, 0.25, 0.25),
-                   unimpaired_preds, other_preds, mci_preds, 
-                   categorical_vars = W, continuous_vars = Z, id_var = "HHIDPN", 
-                   variable_labels, dataset_to_copy, cell_ID_key, color_palette,
-                   num_synthetic = 10, unimpaired_betas, unimpaired_cov, 
-                   other_betas, other_cov, mci_betas, mci_cov, alpha_0_dist, 
-                   prior_Sigma, prior_V_inv, prior_beta, nu_0, kappa_0, 
-                   contrasts_matrix = A,
-                   path_to_analyses_folder = 
-                     paste0(path_to_box, "analyses/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"), 
-                   path_to_figures_folder = 
-                     paste0(path_to_box, "figures/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"))
+for(sim in 1:5){
+  generate_synthetic(warm_up = 2, run_number = paste0("sim_", sim), 
+                     starting_props = c(0.25, 0.25, 0.25, 0.25),
+                     unimpaired_preds, other_preds, mci_preds, 
+                     categorical_vars = W, continuous_vars = Z, 
+                     id_var = "HHIDPN", variable_labels, dataset_to_copy, 
+                     cell_ID_key, color_palette, num_synthetic = 10, 
+                     unimpaired_betas, unimpaired_cov, other_betas, other_cov, 
+                     mci_betas, mci_cov, alpha_0_dist, prior_Sigma, prior_V_inv, 
+                     prior_beta, nu_0, kappa_0, contrasts_matrix = A,
+                     path_to_analyses_folder = 
+                       paste0(path_to_box, "analyses/simulation_study/", 
+                              "HCAP_normal_250_unimpaired/"), 
+                     path_to_figures_folder = 
+                       paste0(path_to_box, "figures/simulation_study/", 
+                              "HCAP_normal_250_unimpaired/"))
+}
+
+#---- 
+
