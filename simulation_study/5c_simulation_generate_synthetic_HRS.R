9504657ec1f050b974e74044254b64bb8817abd4
cshawsome <c.shaw@ucla.edu>
2022-03-28 20:23:42 -0700

    add packages add weights and predicted dementia status to synthetic data

--- simulation_study/5c_simulation_generate_synthetic_HRS.R
+++ simulation_study/5c_simulation_generate_synthetic_HRS.R
@@ -11,6 +11,12 @@ source(here::here("functions", "read_results.R"))
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
+#---- **synthetic true HRS----
+simulation_truth <-  
+  read_csv(paste0(path_to_box, "analyses/simulation_study/synthetic_data/HRS/", 
+                  "HRS_synthetic_normal_500_unimpaired.csv")) 
+
+#---- **synthetic HCAP ----
 simulated_data_directories <- 
   list.dirs(path = paste0(path_to_box, 
                           "analyses/simulation_study/HCAP_normal_250_unimpaired/", 
@@ -31,3 +37,22 @@ for(path in simulated_data_directories){
     simulated_data %<>% append(., lapply(simulated_data_paths, read_results))
   }
 }
+
+#---- ****add weights ----
+#the only variable that contributed to selection was marital status
+# we selected half of married people and half of unmarried people, thus the 
+# weight for every observation should be 2
+
+simulated_data <- 
+  lapply(simulated_data, function(x) x %<>% mutate("weight" = 2))
+
+#---- ****predicted dementia status ----
+simulated_data <- 
+  lapply(simulated_data, function(x) x %<>% 
+           mutate("predicted_Dementia" = ifelse(Group == "Dementia", 1, 0)))
+
+#---- models ----
+test <- with(simulated_data[[1]], 
+             glm(predicted_Dementia ~ age_Z + female + black + hispanic, 
+                 family = "poisson", weights = weight) %>% 
+               tidy(., conf.int = TRUE, conf.level = 0.95, exponentiate = TRUE))
