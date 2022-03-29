f7718a8fb76d30947cadeeaf978f601c26a3509f
cshawsome <c.shaw@ucla.edu>
2022-03-29 13:52:53 -0700

    set return values for function

--- functions/read_results.R
+++ functions/read_results.R
@@ -3,8 +3,8 @@ read_results <- function(path){
   if(str_detect(path, "HRS_synthetic")){
     dataset_name <- str_split(path, pattern = "HRS_synthetic_")[[1]][2]
     dataset_name <- str_remove(dataset_name, pattern = ".csv")
-    data.table::fread(path, fill = TRUE) %>% 
-      mutate("dataset_name" = dataset_name)
+    return(data.table::fread(path, fill = TRUE) %>% 
+      mutate("dataset_name" = dataset_name))
   }
   
   #---- simulation study results ----
@@ -19,9 +19,9 @@ read_results <- function(path){
     dataset_number <- str_split(dataset_number, pattern = "/")[[1]][2]
     dataset_number <- str_remove(dataset_number, pattern = ".csv")
     
-    data.table::fread(path, fill = TRUE) %>% 
+    return(data.table::fread(path, fill = TRUE) %>% 
       mutate("dataset_name" = dataset_name, 
              "sim_name" = sim_name, 
-             "dataset_number" = dataset_number)
+             "dataset_number" = dataset_number))
   }
 }
