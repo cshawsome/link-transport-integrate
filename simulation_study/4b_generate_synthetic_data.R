f21089676c98685ccface4093f27def8f89bdd16
cshawsome <c.shaw@ucla.edu>
2022-04-05 11:49:31 -0700

    use apply to generate synthetic data for all ADAMS prop normal scenarios

--- simulation_study/4b_generate_synthetic_data.R
+++ simulation_study/4b_generate_synthetic_data.R
@@ -83,18 +83,31 @@ kappa_0 <- c(0.85, 0.85, 0.85, 0.85) %>%
   set_names(c("Unimpaired", "MCI", "Dementia", "Other"))
 
 #---- generate synthetic data ----
-generate_synthetic(warm_up = 100, run_number = 1, 
-                   starting_props = c(0.25, 0.25, 0.25, 0.25),
-                   unimpaired_preds, other_preds, mci_preds, 
-                   categorical_vars = W, continuous_vars = Z, id_var = "HHIDPN", 
-                   variable_labels, dataset_to_copy, cell_ID_key, color_palette,
-                   num_synthetic = 1000, unimpaired_betas, unimpaired_cov, 
-                   other_betas, other_cov, mci_betas, mci_cov, alpha_0_dist, 
-                   prior_Sigma, prior_V_inv, prior_beta, nu_0, kappa_0, 
-                   contrasts_matrix = A,
-                   path_to_analyses_folder = 
-                     paste0(path_to_box, "analyses/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"), 
-                   path_to_figures_folder = 
-                     paste0(path_to_box, "figures/simulation_study/", 
-                            "HCAP_normal_250_unimpaired/"))
+set.seed(20220329)
+start <- Sys.time()
+#---- **normal data ----
+lapply(synthetic_data_list, function(x)
+  generate_synthetic(warm_up = 100, run_number = 1, 
+                     starting_props = c(0.25, 0.25, 0.25, 0.25),
+                     unimpaired_preds, other_preds, mci_preds, 
+                     categorical_vars = W, continuous_vars = Z, id_var = "HHIDPN", 
+                     variable_labels, dataset_to_copy = x %>% 
+                       group_by(married_partnered) %>% 
+                       slice_sample(prop = 0.5) %>% 
+                       mutate("(Intercept)" = 1) %>% ungroup(), cell_ID_key, 
+                     color_palette, num_synthetic = 1000, unimpaired_betas, 
+                     unimpaired_cov, other_betas, other_cov, mci_betas, mci_cov, 
+                     alpha_0_dist, prior_Sigma, prior_V_inv, prior_beta, nu_0, 
+                     kappa_0, contrasts_matrix = A,
+                     path_to_analyses_folder = 
+                       paste0(path_to_box, "analyses/simulation_study/HCAP_HRS_", 
+                              unique(x[, "dataset_name"]), "/"), 
+                     path_to_figures_folder = 
+                       paste0(path_to_box,
+                              "figures/simulation_study/HCAP_HRS_", 
+                              unique(x[, "dataset_name"]), "/")))
+  
+end <- Sys.time() - start
+
+
+
