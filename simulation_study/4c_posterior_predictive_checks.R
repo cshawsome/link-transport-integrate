452ebeb6f20287b3e81261124e6b67c032e624f3
cshawsome <c.shaw@ucla.edu>
2022-04-12 11:19:04 -0700

    add read_results function and filter down to two datasets for now

--- simulation_study/4c_posterior_predictive_checks.R
+++ simulation_study/4c_posterior_predictive_checks.R
@@ -8,11 +8,27 @@ p_load("tidyverse", "DirichletReg", "magrittr", "here", "MASS", "MCMCpack",
        "qdapRegex")
 
 #---- source functions ----
+source(here::here("functions", "read_results.R"))
 source(here::here("simulation_study", "functions", 
                   "posterior_predictive_checks_function.R"))
 
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+
+#---- **data paths ----
+synthetic_data_paths <- 
+  list.files(path = paste0(path_to_box, 
+                           "analyses/simulation_study/synthetic_data/HRS"), 
+             full.names = TRUE, pattern = "*.csv")
+
+#subset for now
+synthetic_data_paths <- 
+  synthetic_data_paths[
+    str_detect(synthetic_data_paths, 
+               "synthetic_normal_500_ADAMS|synthetic_normal_1000_ADAMS")]
+
+
+synthetic_data_list <- lapply(synthetic_data_paths, read_results)
 
 #---- **dataset to copy ----
 #dataset we're trying to create synthetic versions of
