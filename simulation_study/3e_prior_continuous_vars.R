6f8a01ee4f577d128b33186ea3da2259e00e0f48
cshawsome <c.shaw@ucla.edu>
2022-04-19 14:43:12 -0700

    save priors as a nested list

--- simulation_study/3e_prior_continuous_vars.R
+++ simulation_study/3e_prior_continuous_vars.R
@@ -42,8 +42,8 @@ Z <- selected_vars[str_detect(selected_vars, "Z")]
 
 #---- **prior: cell counts ----
 alpha_0_dist <- 
-  read_csv(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
-                  "imputation_cell_props.csv")) 
+  readRDS(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
+                 "imputation_cell_props")) 
 
 #---- **contrasts matrix ----
 #categorical vars contrasts matrix
@@ -58,22 +58,19 @@ cells <- A %>% as.data.frame() %>% unite("cells", -1, sep = "") %>%
 estimate_cont_priors <- function(data, W, Z, A){
   #---- **pre-allocate ----
   priors_V_inv <- 
-    as.data.frame(matrix(nrow = (ncol(A)*ncol(A)*4), ncol = 1)) %>% 
-    set_colnames("V_inv") %>% 
+    as.data.frame(matrix(nrow = (ncol(A)*4), ncol = ncol(A))) %>%
     mutate("group" = rep(c("Unimpaired", "MCI", "Dementia", "Other"), 
-                         each = ncol(A)*ncol(A))) 
+                         each = ncol(A))) 
   
   priors_beta <- 
-    as.data.frame(matrix(nrow = (length(Z)*ncol(A)*4) , ncol = 1)) %>% 
-    set_colnames("beta") %>% 
+    as.data.frame(matrix(nrow = (ncol(A)*4) , ncol = length(Z))) %>% 
     mutate("group" = rep(c("Unimpaired", "MCI", "Dementia", "Other"), 
-                         each = length(Z)*ncol(A)))
+                         each = ncol(A)))
   
   priors_Sigma <- 
-    as.data.frame(matrix(nrow = (length(Z)*length(Z)*4), ncol = 1)) %>% 
-    set_colnames("Sigma") %>% 
+    as.data.frame(matrix(nrow = (length(Z)*4), ncol = length(Z))) %>% 
     mutate("group" = rep(c("Unimpaired", "MCI", "Dementia", "Other"), 
-                         each = length(Z)*length(Z))) 
+                         each = length(Z))) 
   
   for(class in c("Unimpaired", "MCI", "Other", "Dementia")){
     #---- **filter data ----
@@ -106,42 +103,62 @@ estimate_cont_priors <- function(data, W, Z, A){
     
     UtU <- diag(unlist(contingency_table[, "Freq"]))
     
-    #---- **draw new UtU if needed ----
-    while(det(t(A) %*% UtU %*% A) < 1e-9){
-      max_index <- 
-        colnames(alpha_0_dist)[str_detect(colnames(alpha_0_dist), "[0-9]+")] %>% 
-        as.numeric() %>% max()
-      random_draw <- sample(seq(1, max_index), size = 1)
-      new_props <- alpha_0_dist[, c(as.character(random_draw), "group")] %>% 
-        filter(group == class)
-      
-      UtU <- diag(round(unlist(new_counts[, 1])*nrow(subset)))
-    }
+    #---- NEEDS TO BE REFACTORED ----
+    # #---- **draw new UtU if needed ----
+    # while(det(t(A) %*% UtU %*% A) < 1e-9){
+    #   max_index <- 
+    #     colnames(alpha_0_dist)[str_detect(colnames(alpha_0_dist), "[0-9]+")] %>% 
+    #     as.numeric() %>% max()
+    #   random_draw <- sample(seq(1, max_index), size = 1)
+    #   new_props <- alpha_0_dist[, c(as.character(random_draw), "group")] %>% 
+    #     filter(group == class)
+    #   
+    #   UtU <- diag(round(unlist(new_counts[, 1])*nrow(subset)))
+    # }
     
     #---- **beta hat ----
     continuous_covariates <- subset %>% dplyr::select(all_of(Z)) %>% as.matrix
     
     V_inv <- t(A) %*% UtU %*% A
     V <- solve(V_inv)
-    priors_V_inv[which(priors_V_inv$group == class), "V_inv"] <- 
-      as.vector(V_inv)
+    priors_V_inv[which(priors_V_inv$group == class), 
+                 seq(1, ncol(V_inv))] <- V_inv
     
     beta_hat <- V %*% t(A) %*% t(U) %*% continuous_covariates
-    priors_beta[which(priors_beta$group == class), "beta"] <- 
-      as.vector(beta_hat)
+    priors_beta[which(priors_beta$group == class), seq(1, ncol(beta_hat))] <- 
+      beta_hat
     
     #---- **Sigma hat ----
     residual <- continuous_covariates - U %*% A %*% beta_hat
-    priors_Sigma[which(priors_Sigma$group == class), "Sigma"] <- 
-      as.vector(t(residual) %*% residual)
+    priors_Sigma[which(priors_Sigma$group == class), seq(1, ncol(residual))] <- 
+      t(residual) %*% residual
   }
   
   #---- **return values ----
-  return(list(priors_V_inv, priors_beta, priors_Sigma))
+  return(list("priors_V_inv" = priors_V_inv %<>% group_by(group) %>% 
+                group_split() %>% 
+                set_names(c("Dementia", "MCI", "Other", "Unimpaired")), 
+              "priors_beta" = priors_beta %<>% group_by(group) %>% 
+                group_split() %>% 
+                set_names(c("Dementia", "MCI", "Other", "Unimpaired")), 
+              "priors_Sigma" = priors_Sigma %<>% group_by(group) %>% 
+                group_split() %>% 
+                set_names(c("Dementia", "MCI", "Other", "Unimpaired"))))
 }
 
 #---- **estimate values ----
 all_priors <- lapply(prior_imputed_clean, estimate_cont_priors, W, Z, A)
+
+#---- format output ----
+for(est in c("V_inv", "beta", "Sigma")){
+  
+  data <- lapply(all_priors, "[[", paste0("priors_", est)) 
+  
+  saveRDS(data, paste0(path_to_box, "analyses/simulation_study/prior_data/", 
+                       "priors_", est))
+}
+
+#---- OLD ----
 
 #---- **pre-allocate ----
 priors_V_inv <- 
