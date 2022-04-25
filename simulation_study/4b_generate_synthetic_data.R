086d3c872e2ee1aef596a996e28d5b151dcc71c1
cshawsome <c.shaw@ucla.edu>
2022-04-25 16:31:50 -0700

    generate data for all simulation scenarios

--- simulation_study/4b_generate_synthetic_data.R
+++ simulation_study/4b_generate_synthetic_data.R
@@ -21,12 +21,6 @@ synthetic_data_paths <-
                            "analyses/simulation_study/synthetic_data/HRS"), 
              full.names = TRUE, pattern = "*.csv")
 
-#Focus on full analysis with normal, ADAMS props data
-synthetic_data_paths <- 
-  synthetic_data_paths[unlist(lapply(synthetic_data_paths, 
-                                     function(x) str_detect(x, "ADAMS") & 
-                                       str_detect(x, "_normal")))]
-
 synthetic_data_list <- lapply(synthetic_data_paths, read_results)
 
 #---- **variable labels ----
@@ -85,11 +79,12 @@ kappa_0 <- c(0.85, 0.85, 0.85, 0.85) %>%
   set_names(c("Unimpaired", "MCI", "Dementia", "Other"))
 
 #---- generate synthetic data ----
+#41 mins to generate one copy of each dataset
 set.seed(20220329)
 start <- Sys.time()
 plan(multisession, workers = (availableCores() - 2))
 
-future_lapply(synthetic_data_list[1:2], function(x)
+future_lapply(synthetic_data_list, function(x)
   generate_synthetic(warm_up = 100, run_number = 1, 
                      starting_props = c(0.25, 0.25, 0.25, 0.25),
                      unimpaired_preds, other_preds, mci_preds, 
@@ -115,75 +110,3 @@ future_lapply(synthetic_data_list[1:2], function(x)
 
 end <- Sys.time() - start
 plan(sequential)
-
-#---- code testing ----
-#---- **test 1: serial; one at a time ----
-#n = 500 dataset (1.05 mins laptop)
-#n = 1000 dataset (XX mins desktop, 1.13 min laptop)
-#n = 2000 dataset (XX mins desktop, XX min laptop)
-#n = 4000 dataset (XX mins desktop, XX min laptop)
-#n = 8000 dataset (XX mins desktop, XX min laptop)
-#
-#Just data generation
-#n = 500 dataset 
-#n = 1000 dataset (0.8 mins laptop)
-#n = 2000 dataset 
-#n = 4000 dataset
-#n = 8000 dataset
-set.seed(20220329)
-start <- Sys.time()
-generate_synthetic(warm_up = 100, run_number = 1, 
-                   starting_props = c(0.25, 0.25, 0.25, 0.25),
-                   unimpaired_preds, other_preds, mci_preds, 
-                   categorical_vars = W, continuous_vars = Z, 
-                   id_var = "HHIDPN", variable_labels, 
-                   dataset_to_copy = synthetic_data_list[[5]] %>% 
-                     group_by(married_partnered) %>% 
-                     slice_sample(prop = 0.5) %>% 
-                     mutate("(Intercept)" = 1) %>% ungroup(), cell_ID_key, 
-                   color_palette, num_synthetic = 1000, unimpaired_betas, 
-                   unimpaired_cov, other_betas, other_cov, mci_betas, mci_cov, 
-                   alpha_0_dist, prior_Sigma, prior_V_inv, prior_beta, nu_0, 
-                   kappa_0, contrasts_matrix = A,
-                   path_to_analyses_folder = 
-                     paste0(path_to_box, "analyses/simulation_study/HCAP_HRS_", 
-                            unique(synthetic_data_list[[5]][, "dataset_name"]), 
-                            "/"), 
-                   path_to_figures_folder = 
-                     paste0(path_to_box,
-                            "figures/simulation_study/HCAP_HRS_", 
-                            unique(synthetic_data_list[[5]][, "dataset_name"]), 
-                            "/"), data_only = FALSE)
-end <- Sys.time() - start
-
-#---- **test 2: parallel ----
-set.seed(20220329)
-start <- Sys.time()
-plan(multisession, workers = (availableCores() - 2))
-
-future_lapply(synthetic_data_list[1:2], function(x)
-  generate_synthetic(warm_up = 100, run_number = 1, 
-                     starting_props = c(0.25, 0.25, 0.25, 0.25),
-                     unimpaired_preds, other_preds, mci_preds, 
-                     categorical_vars = W, continuous_vars = Z, 
-                     id_var = "HHIDPN", variable_labels, 
-                     dataset_to_copy = x %>% 
-                       group_by(married_partnered) %>% 
-                       slice_sample(prop = 0.5) %>% 
-                       mutate("(Intercept)" = 1) %>% ungroup(), cell_ID_key, 
-                     color_palette, num_synthetic = 1000, unimpaired_betas, 
-                     unimpaired_cov, other_betas, other_cov, mci_betas, mci_cov, 
-                     alpha_0_dist, prior_Sigma, prior_V_inv, prior_beta, nu_0, 
-                     kappa_0, contrasts_matrix = A,
-                     path_to_analyses_folder = 
-                       paste0(path_to_box, "analyses/simulation_study/HCAP_HRS_", 
-                              unique(x[, "dataset_name"]), "/"), 
-                     path_to_figures_folder = 
-                       paste0(path_to_box,
-                              "figures/simulation_study/HCAP_HRS_", 
-                              unique(x[, "dataset_name"]), "/")), 
-  future.seed = TRUE)
-end <- Sys.time() - start
-plan(sequential)
-
-
