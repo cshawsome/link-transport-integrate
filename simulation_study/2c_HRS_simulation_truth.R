54317c485a9a1f438be82e3f3c95ef8ed4b52df8
cshawsome <c.shaw@ucla.edu>
2022-05-01 13:19:43 -0700

    update data name

--- simulation_study/2c_HRS_simulation_truth.R
+++ simulation_study/2c_HRS_simulation_truth.R
@@ -11,12 +11,13 @@ source(here::here("functions", "read_results.R"))
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
-synthetic_data_paths <- 
+superpopulations_paths <- 
   list.files(path = paste0(path_to_box, 
-                           "analyses/simulation_study/synthetic_data/HRS"), 
+                           "analyses/simulation_study/superpopulations"), 
              full.names = TRUE, pattern = "*.csv")
 
-synthetic_data <- do.call(rbind, lapply(synthetic_data_paths, read_results))
+superpopulations_data <- 
+  do.call(rbind, lapply(superpopulations_paths, read_results))
 
 #---- analytic models ----
 for(dataset in unique(synthetic_data$dataset_name)){
