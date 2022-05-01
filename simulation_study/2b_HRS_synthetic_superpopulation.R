33d2139d46a62df368fc6b542635b4cc3a63d64d
cshawsome <c.shaw@ucla.edu>
2022-04-30 23:42:59 -0700

    generate synthetic population for each distribution and time code

--- simulation_study/2b_HRS_synthetic_superpopulation.R
+++ simulation_study/2b_HRS_synthetic_superpopulation.R
@@ -38,35 +38,36 @@ HRS_analytic %<>% mutate("Intercept" = 1) %>%
 
 #---- synthetic data ----
 set.seed(20220303)
-for(n in c(500, 1000, 2000, 4000, 8000)){
-  for(dist_name in c("normal", "lognormal", "bathtub")){
-    #---- ****compare with ADAMS ----
-    generate_synthetic_continuous(HRS_analytic, sample_size = n, 
-                                  unimpaired_prop = 0.35, mci_prop = 0.10, 
-                                  dementia_prop = 0.35, dist = dist_name, 
-                                  parameters = normal_parameter_list, 
-                                  scenario_name = "ADAMS",
-                                  path_to_results = 
-                                    paste0(path_to_box, "analyses/", 
-                                           "simulation_study/superpopulations/"))
-    #---- ****mostly unimpaired ----
-    generate_synthetic_continuous(HRS_analytic, sample_size = n, 
-                                  unimpaired_prop = 0.50, mci_prop = 0.20, 
-                                  dementia_prop = 0.20, dist = dist_name, 
-                                  parameters = normal_parameter_list, 
-                                  path_to_results = 
-                                    paste0(path_to_box, "analyses/", 
-                                           "simulation_study/superpopulations/")) 
-    #---- ****mostly dementia ----
-    generate_synthetic_continuous(HRS_analytic, sample_size = n, 
-                                  unimpaired_prop = 0.20, mci_prop = 0.20, 
-                                  dementia_prop = 0.50, dist = dist_name, 
-                                  parameters = normal_parameter_list, 
-                                  path_to_results = 
-                                    paste0(path_to_box, "analyses/", 
-                                           "simulation_study/superpopulations/")) 
-  }
+
+for(dist_name in c("normal", "lognormal", "bathtub")){
+  #---- ****compare with ADAMS ----
+  generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
+                                unimpaired_prop = 0.35, mci_prop = 0.10, 
+                                dementia_prop = 0.35, dist = dist_name, 
+                                parameters = normal_parameter_list, 
+                                selected_vars_estimates = selected_vars_betas,
+                                scenario_name = "ADAMS",
+                                path_to_results = 
+                                  paste0(path_to_box, "analyses/", 
+                                         "simulation_study/superpopulations/"))
+  #---- ****mostly unimpaired ----
+  generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
+                                unimpaired_prop = 0.50, mci_prop = 0.20, 
+                                dementia_prop = 0.20, dist = dist_name, 
+                                parameters = normal_parameter_list, 
+                                path_to_results = 
+                                  paste0(path_to_box, "analyses/", 
+                                         "simulation_study/superpopulations/")) 
+  #---- ****mostly dementia ----
+  generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
+                                unimpaired_prop = 0.20, mci_prop = 0.20, 
+                                dementia_prop = 0.50, dist = dist_name, 
+                                parameters = normal_parameter_list, 
+                                path_to_results = 
+                                  paste0(path_to_box, "analyses/", 
+                                         "simulation_study/superpopulations/")) 
 }
+
 
 
 
