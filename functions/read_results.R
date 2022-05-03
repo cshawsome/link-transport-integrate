6cf026ef765a409bd586734b38e006eb19ef381b
cshawsome <c.shaw@ucla.edu>
2022-05-03 09:48:30 -0700

    data read for extra analyses (variances of datasets)

--- functions/read_results.R
+++ functions/read_results.R
@@ -25,6 +25,14 @@ read_results <- function(path){
     return(data)
   }
   
+  #---- extra_analyses ----
+  if(str_detect(path, "HRS_synthetic")){
+    dataset_name <- str_split(path, pattern = "HRS_synthetic_")[[1]][2]
+    dataset_name <- str_remove(dataset_name, pattern = ".csv")
+    return(data.table::fread(path, fill = TRUE) %>% 
+             mutate("dataset_name" = dataset_name))
+  }
+  
   #---- OLD ----
   # #---- simulation study results ----
   # if(str_detect(path, "HCAP_")){
