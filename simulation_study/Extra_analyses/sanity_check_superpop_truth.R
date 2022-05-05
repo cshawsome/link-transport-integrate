7969751d13f18385a93fffd972e385c6625c85ef
cshawsome <c.shaw@ucla.edu>
2022-05-04 22:46:18 -0700

    try generating superpopulation based on imputed HRS

--- simulation_study/Extra_analyses/sanity_check_superpop_truth.R
+++ simulation_study/Extra_analyses/sanity_check_superpop_truth.R
@@ -3,7 +3,8 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "magrittr", "here", "broom", "miceFast", "ggforce")
+p_load("tidyverse", "magrittr", "here", "broom", "miceFast", "ggforce", 
+       "locfit", "LaplacesDemon")
 
 options(scipen = 999)
 
@@ -28,6 +29,8 @@ glm(Dementia ~ age_Z + Female + Black + Hispanic + ANMSETOT,
 #---- try one imputation of superpop ----
 #---- **source functions ----
 source(here::here("functions", "fast_impute.R"))
+source(here("simulation_study", "functions", 
+            "generate_synthetic_continuous_function.R"))
 
 #---- **read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
@@ -64,9 +67,59 @@ fast_impute(predictor_matrix = predict, data = HRS_clean,
             method = "PMM", m = 1, maxit = 15, chunk = 1)
 end <- Sys.time() - start
 
-#---- read in results ----
+#---- **read in results ----
 HRS_imputed <- readRDS(paste0(path_to_box, 
-                              "data/HRS/cleaned/MI/chunk_1/MI_datasets"))
+                              "data/HRS/cleaned/MI/chunk_1/MI_datasets")) %>% 
+  do.call(rbind, .)
+
+#---- **generate synthetic pop ----
+#---- ****selected vars betas ----
+selected_vars_betas <- 
+  read_csv(paste0(path_to_box, "analyses/simulation_study/variable_selection/", 
+                  "selected_vars_model_coefficients.csv"))
+
+#---- ****variable labels ----
+variable_labels <- 
+  read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) %>% 
+  filter(HRS %in% colnames(HRS_imputed)) 
+
+#---- **continuous distribution parameters ----
+normal_parameter_list <- 
+  readRDS(paste0(path_to_box, "analyses/simulation_study/", 
+                 "continuous_distribution_parameters/", 
+                 "normal_parameters"))
+
+#---- **format data ----
+HRS_imputed %<>% 
+  rename_at(vars(variable_labels$HRS), ~ variable_labels$data_label) %>% 
+  rename("Intercept" = "intercept")
+
+#---- **synthetic data ----
+set.seed(20220303)
+
+start <- Sys.time()
+for(dist_name in c("normal")){
+  #---- ****compare with ADAMS ----
+  generate_synthetic_continuous(HRS_imputed, sample_size = 1000000, 
+                                unimpaired_prop = 0.35, mci_prop = 0.10, 
+                                dementia_prop = 0.35, dist = dist_name, 
+                                parameters = normal_parameter_list, 
+                                selected_vars_estimates = selected_vars_betas,
+                                scenario_name = "ADAMS",
+                                path_to_results = 
+                                  paste0(path_to_box, "analyses/", 
+                                         "simulation_study/", 
+                                         "test_superpopulations/"))
+}
+
+end <- Sys.time() - start
+
+#---- **test model ----
+glm(Dementia ~ age_Z + Female + Black + Hispanic, 
+    family = "poisson", data = ADAMS_analytic) %>% 
+  tidy(., conf.int = TRUE, conf.level = 0.95, exponentiate = FALSE) %>% 
+  mutate("RR" = exp(estimate))
+
 
 
 
