3ac16303b0c581efc0b6c0b02d60c4a9633ee979
cshawsome <c.shaw@ucla.edu>
2022-05-07 15:50:07 -0700

    changed to fixed betas for superpop updated parameters for generate_synthetic_continuous function

--- simulation_study/2b_HRS_synthetic_superpopulation.R
+++ simulation_study/2b_HRS_synthetic_superpopulation.R
@@ -14,10 +14,10 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 HRS_analytic <- 
   read_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_analytic.csv"))
 
-#---- **selected vars betas ----
-selected_vars_betas <- 
+#---- **fixed betas ----
+fixed_betas <- 
   read_csv(paste0(path_to_box, "analyses/simulation_study/variable_selection/", 
-                  "selected_vars_model_coefficients.csv"))
+                  "fixed_model_coefficients.csv"))
 
 #---- **variable labels ----
 variable_labels <- 
@@ -41,34 +41,34 @@ HRS_analytic %<>% mutate("Intercept" = 1) %>%
 #---- synthetic data ----
 set.seed(20220303)
 
-#About 40 mins
+#About 3.3 hours
 start <- Sys.time()
 for(dist_name in c("normal", "lognormal", "bathtub")){
   #---- ****compare with ADAMS ----
   generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
-                                unimpaired_prop = 0.35, mci_prop = 0.10, 
-                                dementia_prop = 0.35, dist = dist_name, 
+                                dementia_prop = 0.35, mci_prop = 0.10, 
+                                other_prop = 0.20, dist = dist_name, 
                                 parameters = normal_parameter_list, 
-                                selected_vars_estimates = selected_vars_betas,
+                                selected_vars_estimates = fixed_betas,
                                 scenario_name = "ADAMS",
                                 path_to_results = 
                                   paste0(path_to_box, "analyses/", 
                                          "simulation_study/superpopulations/"))
   #---- ****mostly unimpaired ----
   generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
-                                unimpaired_prop = 0.50, mci_prop = 0.20, 
-                                dementia_prop = 0.20, dist = dist_name, 
+                                dementia_prop = 0.20, mci_prop = 0.20, 
+                                other_prop = 0.10, dist = dist_name, 
                                 parameters = normal_parameter_list, 
-                                selected_vars_estimates = selected_vars_betas,
+                                selected_vars_estimates = fixed_betas,
                                 path_to_results = 
                                   paste0(path_to_box, "analyses/", 
                                          "simulation_study/superpopulations/")) 
   #---- ****mostly dementia ----
   generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
-                                unimpaired_prop = 0.20, mci_prop = 0.20, 
-                                dementia_prop = 0.50, dist = dist_name, 
+                                dementia_prop = 0.50, mci_prop = 0.20, 
+                                other_prop = 0.10, dist = dist_name, 
                                 parameters = normal_parameter_list, 
-                                selected_vars_estimates = selected_vars_betas,
+                                selected_vars_estimates = fixed_betas,
                                 path_to_results = 
                                   paste0(path_to_box, "analyses/", 
                                          "simulation_study/superpopulations/")) 
