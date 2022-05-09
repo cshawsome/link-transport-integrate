da7360f6704c82e4b777d04d96680d6d4d5add0f
cshawsome <c.shaw@ucla.edu>
2022-05-08 22:26:28 -0700

    remove extra code for tuning

--- simulation_study/4c_generate_synthetic_data.R
+++ simulation_study/4c_generate_synthetic_data.R
@@ -112,34 +112,3 @@ future_lapply(synthetic_data_list, function(x)
 
 end <- Sys.time() - start
 plan(sequential)
-
-#---- generate one set for tuning ----
-set.seed(20220329)
-start <- Sys.time()
-
-lapply(synthetic_data_list[43], function(x)
-  generate_synthetic(warm_up = 100, run_number = 1, 
-                     starting_props = c(0.25, 0.25, 0.25, 0.25),
-                     unimpaired_preds, other_preds, mci_preds, 
-                     categorical_vars = W, continuous_vars = Z, 
-                     id_var = "HHIDPN", variable_labels, 
-                     dataset_to_copy = x %>% 
-                       group_by(married_partnered) %>% 
-                       slice_sample(prop = 0.5) %>% 
-                       mutate("(Intercept)" = 1) %>% ungroup(), cell_ID_key, 
-                     color_palette, num_synthetic = 1000, unimpaired_betas, 
-                     unimpaired_cov, other_betas, other_cov, mci_betas, mci_cov, 
-                     alpha_0_dist, prior_Sigma, prior_V_inv, prior_beta, 
-                     nu_0 = nu_0[, unique(x$dataset_name)], kappa_0_mat, 
-                     contrasts_matrix = A,
-                     path_to_analyses_folder = 
-                       paste0(path_to_box, "analyses/simulation_study/HCAP_HRS_", 
-                              unique(x[, "dataset_name"]), "/"), 
-                     path_to_figures_folder = 
-                       paste0(path_to_box,
-                              "figures/simulation_study/HCAP_HRS_", 
-                              unique(x[, "dataset_name"]), "/"), 
-                     data_only = FALSE))
-
-end <- Sys.time() - start
-
