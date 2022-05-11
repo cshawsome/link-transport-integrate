b50950a0a99e306bc3d394cb4fecad45cb776875
cshawsome <c.shaw@ucla.edu>
2022-05-11 11:47:00 -0700

    .csv of all simulation study scenarios

---
+++ simulation_study/5a_simulation_scenarios.R
@@ -0,0 +1,22 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("tidyverse", "magrittr")
+
+#---- all scenarios ----
+dists <- c("Normal", "Lognormal", "Bathtub")
+samples_sizes <- c(500, 1000, 2000, 4000, 8000)
+prior_props <- c("ADAMS", "Unimpaired", "Dementia")
+
+sim_scenarios <- 
+  as.data.frame(expand.grid(dists, samples_sizes, prior_props)) %>% 
+  set_colnames(c("distribution", "sample_size", "prior_prop"))
+
+#---- **save output ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+
+write_csv(sim_scenarios, paste0(path_to_box, "analyses/simulation_study/", 
+                                "sim_study_scenarios.csv"))
+
