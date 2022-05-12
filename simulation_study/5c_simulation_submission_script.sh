954899fe71177ed84fe637592b97d00fd8be6709
cshawsome <c.shaw@ucla.edu>
2022-05-12 12:23:16 -0700

    request fewer resources for test run

--- simulation_study/5c_simulation_submission_script.sh
+++ simulation_study/5c_simulation_submission_script.sh
@@ -3,7 +3,7 @@
 # error = Merged with joblog
 #$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
 #$ -j y 
-#$ -l h_rt=17:00:00,h_data=4G #requests 17 hours, 4GB of data (per core) 
+#$ -l h_rt=1:00:00,h_data=4G #requests 1 hours, 2GB of data (per core) 
 #$ -pe shared 1 #requests 1 core
 # Email address to notify
 #$ -M $USER@mail #don't change this line, finds your email in the system 
@@ -25,14 +25,14 @@ export OMP_NUM_THREADS=1 #uses max 1 thread (needs to match -pe shared)
 
 #The jobs are combinations of
 #
-#distribution = c("Normal", "Lognormal", "Bathtub")
+#distribution = c("normal", "lognormal", "bathtub")
 #sample_size = c(500, 1000, 2000, 4000, 8000)
-#prior_prop = c("ADAMS", "Unimpaired", "Dementia")
+#prior_prop = c("ADAMS", "unimpaired", "dementia")
 #
 #the task ID indicates the row of the simulation scenario .csv file 
 
 echo "======"
 echo SGE_TASK_ID=$SGE_TASK_ID      
-R CMD BATCH --no-save --no-restore "--args scenario=$SGE_TASK_ID save=\"no\" sens=\"no\" "  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
-echo R CMD BATCH --no-save --no-restore \'--args scenario=$SGE_TASK_ID save=\"no\" sens=\"no\" \'  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
+R CMD BATCH --no-save --no-restore "--args scenario_num=$SGE_TASK_ID "  simulation_run.R output/output.$JOB_ID.$SGE_TASK_ID
+echo R CMD BATCH --no-save --no-restore \'--args scenario_num=$SGE_TASK_ID \'  simulation_run.R output/output.$JOB_ID.$SGE_TASK_ID
 
