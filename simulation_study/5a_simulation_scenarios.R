242bd016af857e5786e9dd05f3a54d47804ad1e4
cshawsome <c.shaw@ucla.edu>
2022-05-17 19:09:28 -0700

    run scenarios in chunks on hoffman

--- simulation_study/5a_simulation_scenarios.R
+++ simulation_study/5a_simulation_scenarios.R
@@ -9,11 +9,18 @@ p_load("tidyverse", "magrittr")
 dists <- c("normal", "lognormal", "bathtub")
 samples_sizes <- c(500, 1000, 2000, 4000, 8000)
 prior_props <- c("ADAMS", "unimpaired", "dementia")
-seeds <- c()
+batch_size <- 20
+total_runs <- 1000
+
+max_batch_count <- total_runs/batch_size
+seeds <- seq(1, max_batch_count)
 
 sim_scenarios <- 
-  as.data.frame(expand.grid(dists, samples_sizes, prior_props)) %>% 
-  set_colnames(c("distribution", "sample_size", "prior_prop"))
+  as.data.frame(expand.grid(dists, samples_sizes, prior_props, seeds)) %>% 
+  set_colnames(c("distribution", "sample_size", "prior_prop", "seed")) %>%
+  arrange(prior_prop, distribution, sample_size, seed) 
+
+sim_scenarios %<>% mutate("job" = seq(1, nrow(sim_scenarios)))
 
 #---- **save output ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
