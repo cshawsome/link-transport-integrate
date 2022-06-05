bb1048191ce9f5a8983c0049bd956c37160ed6b4
cshawsome <c.shaw@ucla.edu>
2022-06-05 00:41:36 -0700

    generate new superpops with cols for algorithm comparison

--- simulation_study/2b_HRS_synthetic_populations_samples.R
+++ simulation_study/2b_HRS_synthetic_populations_samples.R
@@ -39,11 +39,12 @@ HRS_analytic %<>% mutate("Intercept" = 1) %>%
   rename_at(vars(variable_labels$HRS), ~ variable_labels$data_label)
 
 #---- synthetic superpopulations ----
+#Stick with just normal distributions with ADAMS-like proportions of impairment
 set.seed(20220303)
 
 #About 2.2 hours for all superpops
 start <- Sys.time()
-for(dist_name in c("normal", "lognormal", "bathtub")){
+for(dist_name in c("normal")){
   #---- ****compare with ADAMS ----
   generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
                                 dementia_prop = 0.35, mci_prop = 0.10, 
@@ -54,26 +55,49 @@ for(dist_name in c("normal", "lognormal", "bathtub")){
                                 path_to_results = 
                                   paste0(path_to_box, "analyses/", 
                                          "simulation_study/superpopulations/"))
-  #---- ****mostly unimpaired ----
-  generate_synthetic_continuous(HRS_analytic, sample_size = 1000000,
-                                dementia_prop = 0.20, mci_prop = 0.20,
-                                other_prop = 0.10, dist = dist_name,
-                                parameters = normal_parameter_list,
-                                selected_vars_estimates = fixed_betas,
-                                path_to_results =
-                                  paste0(path_to_box, "analyses/",
-                                         "simulation_study/superpopulations/"))
-  #---- ****mostly dementia ----
-  generate_synthetic_continuous(HRS_analytic, sample_size = 1000000,
-                                dementia_prop = 0.50, mci_prop = 0.20,
-                                other_prop = 0.10, dist = dist_name,
-                                parameters = normal_parameter_list,
-                                selected_vars_estimates = fixed_betas,
-                                path_to_results =
-                                  paste0(path_to_box, "analyses/",
-                                         "simulation_study/superpopulations/"))
+  # #---- ****mostly unimpaired ----
+  # generate_synthetic_continuous(HRS_analytic, sample_size = 1000000,
+  #                               dementia_prop = 0.20, mci_prop = 0.20,
+  #                               other_prop = 0.10, dist = dist_name,
+  #                               parameters = normal_parameter_list,
+  #                               selected_vars_estimates = fixed_betas,
+  #                               path_to_results =
+  #                                 paste0(path_to_box, "analyses/",
+  #                                        "simulation_study/superpopulations/"))
+  # #---- ****mostly dementia ----
+  # generate_synthetic_continuous(HRS_analytic, sample_size = 1000000,
+  #                               dementia_prop = 0.50, mci_prop = 0.20,
+  #                               other_prop = 0.10, dist = dist_name,
+  #                               parameters = normal_parameter_list,
+  #                               selected_vars_estimates = fixed_betas,
+  #                               path_to_results =
+  #                                 paste0(path_to_box, "analyses/",
+  #                                        "simulation_study/superpopulations/"))
 }
 end <- Sys.time() - start
+
+#---- comparison algorithms ----
+#Predict dementia using published algorithms
+
+#---- **source functions ----
+source(here::here("functions", "read_results.R"))
+
+#---- **read in superpop data ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+
+#---- **data paths ----
+superpop_data_paths <- 
+  list.files(path = paste0(path_to_box, 
+                           "analyses/simulation_study/superpopulations"), 
+             full.names = TRUE, pattern = "*.csv")
+
+superpop_data_list <- lapply(superpop_data_paths, read_results)
+
+#---- **Herzog-Wallace ----
+#maximizes specificity
+# predictors = HRS Total Cognition (Total score = 35)
+# score cutoff < 8 = Dementia
+
 
 #---- synthetic HRS ----
 #---- **source functions ----
