ee58a4456c03079fc25fbda39788b93e4c0abb6b
cshawsome <c.shaw@ucla.edu>
2022-06-28 15:56:37 +0200

    assign values in global environment

--- simulation_study/functions/specify_priors.R
+++ simulation_study/functions/specify_priors.R
@@ -6,28 +6,31 @@ specify_priors <-
       for(group in c("unimpaired", "mci", "other")){
         assign(paste0(group, "_betas"), 
                vroom(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
-                            "latent_class_", group, "_betas.csv"), delim = ","))
+                            "latent_class_", group, "_betas.csv"), delim = ","), 
+               envir = .GlobalEnv)
         assign(paste0(group, "_cov"), 
                readRDS(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
-                              "latent_class_", group, "_cov")))
+                              "latent_class_", group, "_cov")), 
+               envir = .GlobalEnv)
         
-        assign(paste0(group, "_preds"), get(paste0(group, "_betas"))$preds)
+        assign(paste0(group, "_preds"), get(paste0(group, "_betas"))$preds, 
+               envir = .GlobalEnv)
       }
       
       #---- ****contingency cells ----
-      alpha_0_dist <- 
-        readRDS(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
-                       "imputation_cell_props")) 
+      assign("alpha_0_dist", 
+             readRDS(paste0(path_to_box, "analyses/simulation_study/prior_data/", 
+                            "imputation_cell_props")), envir = .GlobalEnv) 
       
       #--- ****beta and sigma ----
-      priors_beta <- readRDS(paste0(path_to_box, "analyses/simulation_study/",
-                                    "prior_data/priors_beta")) 
-      prior_V_inv <- readRDS(paste0(path_to_box, "analyses/simulation_study/",
-                                    "prior_data/priors_V_inv"))  
-      prior_Sigma <- readRDS(paste0(path_to_box, "analyses/simulation_study/",
-                                    "prior_data/priors_Sigma")) 
+      assign("priors_beta", 
+             readRDS(paste0(path_to_box, "analyses/simulation_study/",
+                            "prior_data/priors_beta")), envir = .GlobalEnv)
+      assign("prior_V_inv", 
+             readRDS(paste0(path_to_box, "analyses/simulation_study/",
+                            "prior_data/priors_V_inv")), envir = .GlobalEnv)  
+      assign("prior_Sigma", 
+             readRDS(paste0(path_to_box, "analyses/simulation_study/",
+                            "prior_data/priors_Sigma")), envir = .GlobalEnv) 
     }
-    
-    #---- return ----
-    return("unimpaired_betas" = unimpaired_betas)
   }
