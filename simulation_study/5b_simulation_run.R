ce26bca2dcdf499d989ff6a5bade798e9cd58d0a
cshawsome <c.shaw@ucla.edu>
2022-08-14 13:30:24 -0700

    update default arguments so we can quickly tell it's not running with parameters

--- simulation_study/5b_simulation_run.R
+++ simulation_study/5b_simulation_run.R
@@ -15,7 +15,7 @@ if(length(args)==0){
   print("No arguments supplied.")
   ##supply default values
   scenario_num = 1
-  num_replicates = 100
+  num_replicates = 1
 }else{
   for(i in 1:length(args)){
     eval(parse(text=args[[i]]))
