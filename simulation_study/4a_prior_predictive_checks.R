3e2120de8a599e76e338b4df04987fa46822a4e2
cshawsome <c.shaw@ucla.edu>
2022-08-17 16:04:14 -0700

    typo for color palette path

--- simulation_study/4a_prior_predictive_checks.R
+++ simulation_study/4a_prior_predictive_checks.R
@@ -23,7 +23,7 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 variable_labels <- read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) 
 
 #---- **impairment class color palette ----
-color_palette <- read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) 
+color_palette <- read_csv(paste0(path_to_box, "data/color_palette.csv")) 
 
 #---- **synthetic HCAP ----
 synthetic_HCAP_list <- 
@@ -68,7 +68,7 @@ indices <-
 
 #---- **run checks ----
 lapply(synthetic_HCAP_list[indices], function(x)
-  prior_predictive_checks(dataset_to_copy = x, calibration_sample = FALSE, 
+  prior_predictive_checks(dataset_to_copy = x, calibration_sample = TRUE, 
                           calibration_prop = 0.50, 
                           calibration_sample_name = "HCAP_50",
                           path_to_raw_prior_sample = 
