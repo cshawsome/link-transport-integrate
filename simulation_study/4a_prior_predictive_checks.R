fdba380a0e7031d197589d786a9fa418fdea855a
cshawsome <c.shaw@ucla.edu>
2022-08-18 14:35:32 -0700

    calibrating new scenarios

--- simulation_study/4a_prior_predictive_checks.R
+++ simulation_study/4a_prior_predictive_checks.R
@@ -64,7 +64,7 @@ start <- Sys.time()
 
 #---- **specify indices ----
 indices <-
-  which(dataset_names %in% paste0("normal_", c(500), "_ADAMS"))
+  which(dataset_names %in% paste0("normal_", c(2000), "_ADAMS"))
 
 #---- **run checks ----
 lapply(synthetic_HCAP_list[indices], function(x)
