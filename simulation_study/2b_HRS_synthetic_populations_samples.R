a6450eb5cc62de894bfe165e80b12d4cfd621f14
cshawsome <c.shaw@ucla.edu>
2022-08-30 12:34:01 -0700

    setting up to rewrite function for creating superpop

--- simulation_study/2b_HRS_synthetic_populations_samples.R
+++ simulation_study/2b_HRS_synthetic_populations_samples.R
@@ -14,25 +14,25 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 HRS_analytic <- 
   read_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_analytic.csv"))
 
-#---- **fixed betas ----
-fixed_betas <- 
-  read_csv(paste0(path_to_box, "analyses/simulation_study/variable_selection/", 
-                  "fixed_model_coefficients.csv"))
+# #---- **fixed betas ----
+# fixed_betas <- 
+#   read_csv(paste0(path_to_box, "data/variable_selection/", 
+#                   "fixed_model_coefficients.csv"))
 
 #---- **variable labels ----
 variable_labels <- 
   read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) %>% 
   filter(HRS %in% colnames(HRS_analytic)) 
 
-#---- **continuous distribution parameters ----
-normal_parameter_list <- 
-  readRDS(paste0(path_to_box, "analyses/simulation_study/", 
-                 "continuous_distribution_parameters/", 
-                 "normal_parameters"))
+# #---- **continuous distribution parameters ----
+# normal_parameter_list <- 
+#   readRDS(paste0(path_to_box, "analyses/simulation_study/", 
+#                  "continuous_distribution_parameters/", 
+#                  "normal_parameters_raw_data"))
 
 #---- source functions ----
 source(here("simulation_study", "functions", 
-            "generate_synthetic_continuous_function.R"))
+            "generate_synthetic_neuropscyh_function.R"))
 
 #---- format data ----
 HRS_analytic %<>% mutate("Intercept" = 1) %>% 
@@ -47,8 +47,8 @@ start <- Sys.time()
 for(dist_name in c("normal")){
   #---- ****compare with ADAMS ----
   generate_synthetic_continuous(HRS_analytic, sample_size = 1000000, 
-                                dementia_prop = 0.35, mci_prop = 0.10, 
-                                other_prop = 0.20, dist = dist_name, 
+                                dementia_prop = 0.20, mci_prop = 0.20, 
+                                other_prop = 0.10, dist = dist_name, 
                                 parameters = normal_parameter_list, 
                                 selected_vars_estimates = fixed_betas,
                                 scenario_name = "ADAMS",
