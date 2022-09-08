eb13e2c906dff01566af75828496899d7b9f9342
cshawsome <c.shaw@ucla.edu>
2022-09-08 11:13:22 -0700

    reorganize so calibration scenarios can be run without changing args

--- simulation_study/5b_model_tuning.R
+++ simulation_study/5b_model_tuning.R
@@ -16,10 +16,6 @@ source(here::here("simulation_study", "functions",
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
-#---- **synthetic HCAP ----
-synthetic_HCAP_list <- 
-  readRDS(paste0(path_to_box, "data/HCAP/synthetic_HCAP_list"))
-
 #---- **variable labels ----
 variable_labels <- read_csv(paste0(path_to_box, "data/variable_crosswalk.csv")) 
 
@@ -51,9 +47,14 @@ kappa_0_mat <-
 
 #---- generate datasets in serial ----
 #About 1.5 hours to generate data for all datasets in serial
+#---- **generate datasets no calibration ----
+#---- ****read in data ----
+synthetic_HCAP_list <- 
+  readRDS(paste0(path_to_box, "data/HCAP/synthetic_HCAP_list"))
 
-#---- **rename datasets based on calibration scenario ----
-calibration_scenario = "HCAP_50"
+#---- ****rename datasets based on calibration scenario ----
+calibration_scenario = "no_calibration"
+
 synthetic_HCAP_list <- 
   lapply(synthetic_HCAP_list, function(x)
     x %<>% mutate("dataset_name_stem" = unlist(unique(x[, "dataset_name"]))))
@@ -64,15 +65,13 @@ synthetic_HCAP_list <-
                     paste0(unlist(unique(x[, "dataset_name_stem"])), "_", 
                            calibration_scenario)))
 
-#---- dataset names ----
+#---- **dataset names ----
 dataset_names <- 
   unlist(lapply(synthetic_HCAP_list, function(x) unique(x$dataset_name)))
 
 #---- **specify indices ----
 indices <-
-  which(dataset_names %in% 
-          paste0("normal_", c(4000), "_ADAMS_", 
-                 calibration_scenario))
+  which(dataset_names %in% paste0("HRS_", c(500), "_", calibration_scenario))
 
 set.seed(20220329)
 start <- Sys.time()
@@ -82,6 +81,62 @@ lapply(synthetic_HCAP_list[indices], function(x)
                      starting_props = c(0.25, 0.25, 0.25, 0.25),
                      dataset_to_copy = x, calibration_sample = TRUE, 
                      calibration_prop = 0.5, calibration_sample_name = "HCAP_50", 
+                     path_to_raw_prior_sample = 
+                       paste0(path_to_box, "data/prior_data/MI/", 
+                              "MI_datasets_cleaned"),
+                     path_to_data = paste0(path_to_box,"data/"), 
+                     path_to_analyses_folder = 
+                       paste0(path_to_box, "analyses/simulation_study/HCAP_HRS_", 
+                              unique(x[, "dataset_name_stem"]), "/"), 
+                     path_to_figures_folder = 
+                       paste0(path_to_box, "figures/simulation_study/HCAP_HRS_", 
+                              unique(x[, "dataset_name_stem"]), "/"), 
+                     categorical_vars = W, continuous_vars = Z, 
+                     id_var = "HHIDPN", variable_labels = variable_labels, 
+                     cell_ID_key = cell_ID_key, color_palette = color_palette, 
+                     contrasts_matrix = A, kappa_0_mat = kappa_0_mat, 
+                     nu_0_mat = nu_0_mat, num_synthetic = 1000, 
+                     data_only = FALSE))
+
+end <- Sys.time() - start
+
+#---- **generate datasets HCAP_50 calibration ----
+#---- ****read in data ----
+synthetic_HCAP_list <- 
+  readRDS(paste0(path_to_box, "data/HCAP/synthetic_HCAP_list"))
+
+#---- ****rename datasets based on calibration scenario ----
+calibration_scenario = "HCAP_50"
+
+synthetic_HCAP_list <- 
+  lapply(synthetic_HCAP_list, function(x)
+    x %<>% mutate("dataset_name_stem" = unlist(unique(x[, "dataset_name"]))))
+
+synthetic_HCAP_list <- 
+  lapply(synthetic_HCAP_list, function(x)
+    x %<>% mutate("dataset_name" = 
+                    paste0(unlist(unique(x[, "dataset_name_stem"])), "_", 
+                           calibration_scenario)))
+
+#---- **dataset names ----
+dataset_names <- 
+  unlist(lapply(synthetic_HCAP_list, function(x) unique(x$dataset_name)))
+
+#---- **specify indices ----
+indices <-
+  which(dataset_names %in% paste0("HRS_", c(500), "_", calibration_scenario))
+
+set.seed(20220329)
+start <- Sys.time()
+
+lapply(synthetic_HCAP_list[indices], function(x)
+  generate_synthetic(warm_up = 100, run_number = 1, 
+                     starting_props = c(0.25, 0.25, 0.25, 0.25),
+                     dataset_to_copy = x, calibration_sample = TRUE, 
+                     calibration_prop = 
+                       as.numeric(str_remove(calibration_scenario, 
+                                             "HCAP_"))/100,
+                     calibration_sample_name = calibration_scenario, 
                      path_to_raw_prior_sample = 
                        paste0(path_to_box, "data/prior_data/MI/", 
                               "MI_datasets_cleaned"),
