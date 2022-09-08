51ff873e606d48983cd914f8b54a09c49ee3d9f2
cshawsome <c.shaw@ucla.edu>
2022-09-08 14:28:36 -0700

    typo in path to output

--- simulation_study/6a_simulation_scenarios.R
+++ simulation_study/6a_simulation_scenarios.R
@@ -24,5 +24,6 @@ sim_scenarios %<>% mutate("job" = seq(1, nrow(sim_scenarios)))
 #---- **save output ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
-write_csv(sim_scenarios, paste0(path_to_box, "data", "sim_study_scenarios.csv"))
+write_csv(sim_scenarios, 
+          paste0(path_to_box, "data/", "sim_study_scenarios.csv"))
 
