f9bb92a497e1c671beddd988da431154905fc04b
cshawsome <c.shaw@ucla.edu>
2022-09-19 11:40:58 -0700

    initial commit

---
+++ ADAMS/analysis_scripts/3_make_tables.R
@@ -0,0 +1,8 @@
+#---- Package Loading and Options ----
+if (!require("pacman")){
+  install.packages("pacman", repos = 'http://cran.us.r-project.org')
+}
+
+p_load("here", "tidyverse", "magrittr", "haven", "stringr", "NormPsy")
+
+options(scipen = 999)
