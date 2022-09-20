43fbf2ae052317b6df4821ae07575df6b4bf19c6
cshawsome <c.shaw@ucla.edu>
2022-09-19 19:14:19 -0700

    read in data

--- ADAMS/analysis_scripts/3_make_tables.R
+++ ADAMS/analysis_scripts/3_make_tables.R
@@ -6,3 +6,9 @@ if (!require("pacman")){
 p_load("here", "tidyverse", "magrittr", "haven", "stringr", "NormPsy")
 
 options(scipen = 999)
+
+#---- read in data ----
+#---- **read in data ----
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+ADAMS_train <- read_csv(paste0(path_to_box, "data/ADAMS/cleaned/ADAMS_train.csv"))
+ADAMS_test <- read_csv(paste0(path_to_box, "data/ADAMS/cleaned/ADAMS_test.csv"))
