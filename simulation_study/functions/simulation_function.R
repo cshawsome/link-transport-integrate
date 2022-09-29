9c4724bee787d42a370eb4a9de61f21593d8e17c
cshawsome <c.shaw@ucla.edu>
2022-09-29 11:14:09 -0700

    start testing function where adjudicated people are included in final counts

--- simulation_study/functions/simulation_function.R
+++ simulation_study/functions/simulation_function.R
@@ -367,76 +367,76 @@ simulation_function <-
     }
   }
 
-# #---- test function ----
-# library("tidyverse")
-# library("DirichletReg")
-# library("magrittr")
-# library("MCMCpack")
-# library("locfit")
-# library("vroom")
-# library("mvnfast")
-# library("mice")
-# library("LaplacesDemon")
-# 
-# path_to_RScripts <- here::here("simulation_study", "functions", "/")
-# source(here::here("functions", "read_results.R"))
-# source(paste0(path_to_RScripts, "generate_synthetic_function.R"))
-# source(paste0(path_to_RScripts, "standardized_dem_estimates.R"))
-# 
-# path_to_data <- paste0("/Users/crystalshaw/Library/CloudStorage/Box-Box/",
-#                        "Dissertation/data/")
-# superpop <-
-#   read_results(paste0(path_to_data, "superpopulations/superpop_1000000.csv"))
-# truth <- read_csv(paste0(path_to_data,
-#                          "superpopulations/agesex_standardized_prevs.csv"))
-# variable_labels <-
-#   read_csv(paste0(path_to_data, "variable_crosswalk.csv"))
-# cell_ID_key <- read_csv(paste0(path_to_data, "cell_ID_key.csv")) %>%
-#   mutate_all(as.character)
-# color_palette <- read_csv(paste0(path_to_data, "color_palette.csv"))
-# all_sim_scenarios <- read_csv(paste0(path_to_data, "sim_study_scenarios.csv"))
-# 
-# warm_up = 100
-# starting_props = rep(0.25, 4)
-# categorical_vars = W = c("black", "hispanic", "stroke")
-# continuous_vars = Z = colnames(superpop)[str_detect(colnames(superpop), "_Z")]
-# id_var = "HHIDPN"
-# scenario = scenario_num = 404 #calibration 50 SRS sample size 2000, HCAP prop 25
-# path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
-# superpopulation <- superpop
-# orig_means = means <-
-#   read_csv(paste0(path_to_box, "data/superpopulations/superpop_means.csv"))
-# orig_sds = sds <-
-#   read_csv(paste0(path_to_box, "data/superpopulations/superpop_sds.csv"))
-# 
-# all_scenarios_list = all_sim_scenarios
-# 
-# num_synthetic = 1000
-# nu_0_mat <- read_csv(paste0(path_to_data, "tuning/nu_0_matrix.csv"))
-# kappa_0_mat <- read_csv(paste0(path_to_data, "tuning/kappa_0_matrix.csv"))
-# contrasts_matrix = A =
-#   read_csv(paste0(path_to_data, "contrasts_matrix.csv")) %>% as.matrix()
-# path_to_results <- paste0(path_to_box, "analyses/simulation_study/results/")
-# seed = 1
-# 
-# set.seed(20220512)
-# 
-# replicate(2,
-#           simulation_function(warm_up = 100, starting_props = rep(0.25, 4),
-#                               categorical_vars = W, continuous_vars = Z,
-#                               id_var = "HHIDPN",
-#                               variable_labels = variable_labels,
-#                               scenario = scenario_num,
-#                               superpopulation = superpop, orig_means = means,
-#                               orig_sds = sds,
-#                               all_scenarios_list = all_sim_scenarios,
-#                               cell_ID_key = cell_ID_key,
-#                               color_palette = color_palette,
-#                               num_synthetic = 1000, contrasts_matrix = A,
-#                               kappa_0_mat = kappa_0_mat, nu_0_mat = nu_0_mat,
-#                               truth = truth, seed = seed,
-#                               path_to_data = path_to_data,
-#                               path_to_results =
-#                                 paste0(path_to_box,
-#                                        "analyses/simulation_study/results/")))
-# 
+#---- test function ----
+library("tidyverse")
+library("DirichletReg")
+library("magrittr")
+library("MCMCpack")
+library("locfit")
+library("vroom")
+library("mvnfast")
+library("mice")
+library("LaplacesDemon")
+
+path_to_RScripts <- here::here("simulation_study", "functions", "/")
+source(here::here("functions", "read_results.R"))
+source(paste0(path_to_RScripts, "generate_synthetic_function.R"))
+source(paste0(path_to_RScripts, "standardized_dem_estimates.R"))
+
+path_to_data <- paste0("/Users/crystalshaw/Library/CloudStorage/Box-Box/",
+                       "Dissertation/data/")
+superpop <-
+  read_results(paste0(path_to_data, "superpopulations/superpop_1000000.csv"))
+truth <- read_csv(paste0(path_to_data,
+                         "superpopulations/agesex_standardized_prevs.csv"))
+variable_labels <-
+  read_csv(paste0(path_to_data, "variable_crosswalk.csv"))
+cell_ID_key <- read_csv(paste0(path_to_data, "cell_ID_key.csv")) %>%
+  mutate_all(as.character)
+color_palette <- read_csv(paste0(path_to_data, "color_palette.csv"))
+all_sim_scenarios <- read_csv(paste0(path_to_data, "sim_study_scenarios.csv"))
+
+warm_up = 100
+starting_props = rep(0.25, 4)
+categorical_vars = W = c("black", "hispanic", "stroke")
+continuous_vars = Z = colnames(superpop)[str_detect(colnames(superpop), "_Z")]
+id_var = "HHIDPN"
+scenario = scenario_num = 404 #calibration 50 SRS sample size 2000, HCAP prop 25
+path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
+superpopulation <- superpop
+orig_means = means <-
+  read_csv(paste0(path_to_box, "data/superpopulations/superpop_means.csv"))
+orig_sds = sds <-
+  read_csv(paste0(path_to_box, "data/superpopulations/superpop_sds.csv"))
+
+all_scenarios_list = all_sim_scenarios
+
+num_synthetic = 1000
+nu_0_mat <- read_csv(paste0(path_to_data, "tuning/nu_0_matrix.csv"))
+kappa_0_mat <- read_csv(paste0(path_to_data, "tuning/kappa_0_matrix.csv"))
+contrasts_matrix = A =
+  read_csv(paste0(path_to_data, "contrasts_matrix.csv")) %>% as.matrix()
+path_to_results <- paste0(path_to_box, "analyses/simulation_study/results/")
+seed = 1
+
+set.seed(20220512)
+
+replicate(2,
+          simulation_function(warm_up = 100, starting_props = rep(0.25, 4),
+                              categorical_vars = W, continuous_vars = Z,
+                              id_var = "HHIDPN",
+                              variable_labels = variable_labels,
+                              scenario = scenario_num,
+                              superpopulation = superpop, orig_means = means,
+                              orig_sds = sds,
+                              all_scenarios_list = all_sim_scenarios,
+                              cell_ID_key = cell_ID_key,
+                              color_palette = color_palette,
+                              num_synthetic = 1000, contrasts_matrix = A,
+                              kappa_0_mat = kappa_0_mat, nu_0_mat = nu_0_mat,
+                              truth = truth, seed = seed,
+                              path_to_data = path_to_data,
+                              path_to_results =
+                                paste0(path_to_box,
+                                       "analyses/simulation_study/results/")))
+
