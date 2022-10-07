20f1caa58d01f323555525737c69d5ec9b819508
cshawsome <c.shaw@ucla.edu>
2022-10-07 10:12:11 -0700

    calibrate new scenario

--- simulation_study/5a_prior_predictive_checks.R
+++ simulation_study/5a_prior_predictive_checks.R
@@ -56,10 +56,10 @@ kappa_0_mat <- read_csv(paste0(path_to_box, "data/tuning/kappa_0_matrix.csv"))
 #---- **user input ----
 #calibration scenario options: "no_calibration", "calibration_50_SRS", 
 # "calibration_50_design", "calibration_100"
-calibration_scenario = "no_calibration" 
+calibration_scenario = "calibration_50_SRS" 
 
 #HCAP sample prop options: 0.25, 0.50
-HCAP_sample_prop = 0.50
+HCAP_sample_prop = 0.25
 
 #---- **read in data ----
 synthetic_HCAP_list <- 
