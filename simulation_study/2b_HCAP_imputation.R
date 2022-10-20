3650c4927ebb5e0af22b496c042531492a88c377
cshawsome <c.shaw@ucla.edu>
2022-10-20 15:25:35 -0700

    impute matrix using PMM from fastmice

--- simulation_study/2b_HCAP_imputation.R
+++ simulation_study/2b_HCAP_imputation.R
@@ -3,7 +3,7 @@ if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
 }
 
-p_load("tidyverse", "here", "magrittr", "tidyr")
+p_load("tidyverse", "here", "magrittr", "tidyr", "miceFast", "ggforce")
 
 #---- read in data ----
 path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
@@ -20,91 +20,49 @@ HCAP <- read_csv(paste0(path_to_box, "data/HCAP/cleaned/HCAP_clean.csv")) %>%
                         "PJ005M1_collapsed_label", "r13drinks_per_week", 
                         "r13drink_cat", "r13drink_cat_label"))
 
-#---- **imputation matrix ----
-hotdeck_vars_mat <- 
-  read_csv(paste0(path_to_box, "data/HCAP/hotdeck_impute_mat.csv")) %>% 
-  column_to_rownames("var_names")
-
 #---- **summarize missingness ----
 #double check that all of these are in the rownames of the imputation matrix
-colMeans(is.na(HCAP))[which(colMeans(is.na(HCAP)) > 0)]
+needs_imputing <- names(colMeans(is.na(HCAP))[which(colMeans(is.na(HCAP)) > 0)])
 
 #---- source functions ----
-source(here::here("simulation_study", "functions", "hotdeck_function.R"))
+source(here::here("functions", "fast_impute.R"))
 
-#---- impute: derive variable bins ----
-HCAP %<>% 
-  mutate(
-    #---- **age ----
-    "age_cat" = cut(age, breaks = c(70, 85, max(HCAP$age)), 
-                    include.lowest = TRUE, right = FALSE), 
-    #---- **education ----
-    "edyrs_cat" = cut(edyrs, 
-                      breaks = c(0, 11, 12, max(HCAP$edyrs)),
-                      include.lowest = TRUE, right = TRUE), 
-    #---- **immediate word recall ----
-    "immrc_cat" = cut(immrc, breaks = c(0, 6, 8, max(HCAP$immrc, na.rm = TRUE)), 
-                      include.lowest = TRUE, right = FALSE),
-    #---- **delayed word recall ----
-    "delrc_cat" = cut(delrc, breaks = c(0, 5, 7, max(HCAP$delrc, na.rm = TRUE)), 
-                      include.lowest = TRUE, right = FALSE),
-    #---- **serial 7s ----
-    "ser7_cat" = cut(ser7, breaks = c(0, 4, max(HCAP$ser7, na.rm = TRUE)), 
-                     include.lowest = TRUE, right = TRUE), 
-    #---- **hrs cognition ----
-    "hrs_cog_HCAP_cat" = cut_number(hrs_cog, n = 5),
-    "hrs_cog_superpop_cat" = cut_number(hrs_cog, n = 4),
-    #---- **mmse_norm ----
-    "mmse_norm_cat" = cut_number(mmse_norm, n = 4),
-    #---- **animal naming ----
-    "animal_naming_cat" = cut_number(animal_naming, n = 5),
-    #---- **word recall yes ----
-    "wrc_yes_cat" = cut(wrc_yes, 
-                        breaks = c(0, 8, max(HCAP$wrc_yes, na.rm = TRUE)), 
-                        include.lowest = TRUE, right = TRUE),
-    #---- **word recall no ----
-    "wrc_no_cat" = cut(wrc_no, 
-                       breaks = c(0, 9, max(HCAP$wrc_no, na.rm = TRUE)), 
-                       include.lowest = TRUE, right = TRUE), 
-    #---- **immediate story recall ----
-    "imm_story_cat" = cut_number(imm_story, n = 4), 
-    #---- **delayed story recall ----
-    "del_story_cat" = cut_number(del_story, n = 5),
-    #---- **immediate constructional praxis ----
-    "imm_cp_cat" = cut(imm_cp, 
-                       breaks = c(0, 7, 10, max(HCAP$imm_cp, na.rm = TRUE)), 
-                       include.lowest = TRUE, right = TRUE),
-    #---- **delayed constructional praxis ----
-    "del_cp_cat" = cut(del_cp, 
-                       breaks = c(0, 4, 6, 8, max(HCAP$imm_cp, na.rm = TRUE)), 
-                       include.lowest = TRUE, right = TRUE),
-    #---- **trails A ----
-    "trailsA_cat" = cut_number(trailsA, n = 5), 
-    #---- **imputed memory ----
-    "memimp16_cat" = cut_number(memimp16, n = 4))
+#---- define imputation var types ----
+not_predictors <- c("HHIDPN", "White", "Other", "Working", "r13no_drinking",
+                    "subj_cog_same")
 
-# #Sanity check bins
-# check_vars <- c("age_cat", "edyrs_cat", "immrc_cat", "delrc_cat", "ser7_cat", 
-#                 "hrs_cog_HCAP_cat", "hrs_cog_superpop_cat", "mmse_norm_cat", 
-#                 "wrc_yes_cat", "wrc_no_cat", "animal_naming_cat", 
-#                 "imm_story_cat", "del_story_cat", "imm_cp_cat", "del_cp_cat", 
-#                 "trailsA_cat", "memimp16_cat")
-# 
-# for(var in check_vars){
-#   print(table(HCAP[, var]))
-# }
+#---- predictor matrix ----
+predict <- 
+  matrix(1, nrow = length(needs_imputing), ncol = ncol(HCAP)) %>% 
+  set_rownames(needs_imputing) %>% set_colnames(colnames(HCAP))
 
-#---- impute: hotdecking ----
-set.seed(20220904)
-HCAP %<>% 
-  hotdeck(dataset_to_impute = ., hotdeck_dataset = HCAP, 
-          imputation_mat = hotdeck_vars_mat, 
-          binary_vars = c("subj_cog_better", "subj_cog_worse", "bwc20", "scissor", 
-                          "cactus", "pres"), 
-          dataset_specific_vars = c("hrs_cog"), dataset_specific_label = "HCAP")
+#---- **cannot predict themselves ----
+predict[needs_imputing, needs_imputing] <- 
+  (diag(x = 1, nrow = length(needs_imputing), 
+        ncol = length(needs_imputing)) == 0)*1
 
-#Sanity check
-colMeans(is.na(HCAP))[which(colMeans(is.na(HCAP)) > 0)]
+#---- **non-predictors ----
+predict[, not_predictors] <- 0
+
+#---- imputation ----
+#About 6 seconds
+set.seed(20220202)
+start <- Sys.time()
+fast_impute(predictor_matrix = predict, data = HCAP, 
+            path_for_output = paste0(path_to_box, "data/HCAP/cleaned/"),
+            method = "PMM", m = 1, maxit = 15, chunk = 1)
+end <- Sys.time() - start
+
+#---- read in results ----
+HCAP_imputed <- 
+  readRDS(paste0(path_to_box, "data/HCAP/cleaned/MI/chunk_1/MI_datasets")) %>% 
+  as.data.frame()
+
+# #Sanity check
+# colMeans(is.na(HCAP_imputed))[which(colMeans(is.na(HCAP_imputed)) > 0)]
+
+#---- OLD ----
+
 
 #---- clean: subjective cog decline vars ----
 HCAP %<>% mutate(subj_cog_count = subj_cog_better + subj_cog_worse)
