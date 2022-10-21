c8a56044cbf53e27c5a518c136fa5d1d5ac71a99
cshawsome <c.shaw@ucla.edu>
2022-10-21 09:15:08 -0700

    remove uneccessary columns

--- simulation_study/3a_HRS_dataset_construction.R
+++ simulation_study/3a_HRS_dataset_construction.R
@@ -311,4 +311,11 @@ HRS %<>%
   rename_at(vars(variable_labels$HRS), ~ variable_labels$data_label)
 
 #---- save dataset ----
-HRS %>% write_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_clean.csv"))
+HRS %>% 
+  dplyr::select(-one_of("GENDER", "RACE", "HISPANIC", "PIWTYPE", "PJ005M1", 
+                        "r13drinkd", "r13drinkn", "r13pstmem", "GENDER_label", 
+                        "RACE_label", "RACE_White", "RACE_Black", "RACE_Other", 
+                        "HISPANIC_indicator", "ETHNIC_label", "PJ005M1_label", 
+                        "PJ005M1_collapsed_label", "drinks_per_week", 
+                        "drink_cat", "drink_cat_label")) %>% 
+  write_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_analytic.csv"))
