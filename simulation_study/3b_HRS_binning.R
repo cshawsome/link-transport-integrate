129983b4e3d2836b321e3fba98d27616ffe1726d
cshawsome <c.shaw@ucla.edu>
2022-10-21 18:20:43 -0700

    vars had already been removed in another script

--- simulation_study/3b_HRS_binning.R
+++ simulation_study/3b_HRS_binning.R
@@ -10,13 +10,7 @@ path_to_box <- "/Users/crystalshaw/Library/CloudStorage/Box-Box/Dissertation/"
 
 #---- **clean HRS ----
 HRS <- 
-  read_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_clean.csv")) %>% 
-  dplyr::select(-one_of("GENDER", "RACE", "HISPANIC", "PIWTYPE", "PJ005M1", 
-                        "r13drinkd", "r13drinkn", "r13pstmem", "GENDER_label", 
-                        "RACE_label", "RACE_White", "RACE_Black", "RACE_Other", 
-                        "HISPANIC_indicator", "ETHNIC_label", "PJ005M1_label", 
-                        "PJ005M1_collapsed_label", "drinks_per_week", 
-                        "drink_cat", "drink_cat_label"))
+  read_csv(paste0(path_to_box, "data/HRS/cleaned/HRS_clean.csv"))
 
 #---- **analytic HCAP ----
 #need HRS bins to match these
